b7942ed97f01e675b09c99ac313d63f114104ffb
Restyled.io <commits@restyled.io>
2020-02-28 16:04:47 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/ops/cond_v2.py
+++ tensorflow/python/ops/cond_v2.py
@@ -18,7 +18,6 @@ This is a version of cond that emits a single If op, as well as the gradient
 function for If ops produced by cond_v2. This will eventually replace the
 current tf.cond implementation once it reaches feature and performance parity.
 """
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
