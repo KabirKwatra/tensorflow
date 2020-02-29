d62bf88a97d04e2fb7937120897239040a74c600
Restyled.io <commits@restyled.io>
2020-02-29 08:04:50 +0000

    Restyled by black

--- tensorflow/python/profiler/profiler_v2_test.py
+++ tensorflow/python/profiler/profiler_v2_test.py
@@ -34,7 +34,6 @@ from tensorflow.python.profiler import traceme
 
 
 class ProfilerTest(test_util.TensorFlowTestCase):
-
     def test_profile_exceptions(self):
         logdir = self.get_temp_dir()
         profiler.start(logdir)
@@ -48,7 +47,7 @@ class ProfilerTest(test_util.TensorFlowTestCase):
     def test_save_profile(self):
         logdir = self.get_temp_dir()
         profiler.start(logdir)
-        with traceme.TraceMe('three_times_five'):
+        with traceme.TraceMe("three_times_five"):
             three = constant_op.constant(3)
             five = constant_op.constant(5)
             product = three * five
@@ -59,36 +58,34 @@ class ProfilerTest(test_util.TensorFlowTestCase):
         self.assertEqual(len(file_list), 2)
         for file_name in gfile.ListDirectory(logdir):
             if gfile.IsDirectory(os.path.join(logdir, file_name)):
-                self.assertEqual(file_name, 'plugins')
+                self.assertEqual(file_name, "plugins")
             else:
-                self.assertTrue(file_name.endswith('.profile-empty'))
-        profile_dir = os.path.join(logdir, 'plugins', 'profile')
+                self.assertTrue(file_name.endswith(".profile-empty"))
+        profile_dir = os.path.join(logdir, "plugins", "profile")
         run = gfile.ListDirectory(profile_dir)[0]
         hostname = socket.gethostname()
-        overview_page = os.path.join(profile_dir, run,
-                                     hostname + '.overview_page.pb')
+        overview_page = os.path.join(profile_dir, run, hostname + ".overview_page.pb")
         self.assertTrue(gfile.Exists(overview_page))
-        input_pipeline = os.path.join(profile_dir, run,
-                                      hostname + '.input_pipeline.pb')
+        input_pipeline = os.path.join(profile_dir, run, hostname + ".input_pipeline.pb")
         self.assertTrue(gfile.Exists(input_pipeline))
-        tensorflow_stats = os.path.join(profile_dir, run,
-                                        hostname + '.tensorflow_stats.pb')
+        tensorflow_stats = os.path.join(
+            profile_dir, run, hostname + ".tensorflow_stats.pb"
+        )
         self.assertTrue(gfile.Exists(tensorflow_stats))
 
-        trace_file = os.path.join(profile_dir, run, hostname + '.trace')
+        trace_file = os.path.join(profile_dir, run, hostname + ".trace")
         self.assertTrue(gfile.Exists(trace_file))
-        with gfile.Open(trace_file, 'rb') as f:
+        with gfile.Open(trace_file, "rb") as f:
             profile_pb = trace_events_pb2.Trace()
             profile_pb.ParseFromString(f.read())
-        devices = frozenset(
-            device.name for device in profile_pb.devices.values())
-        self.assertIn('/host:CPU', devices)
-        if config.list_physical_devices('GPU'):
-            self.assertIn('/device:GPU:0', devices)
+        devices = frozenset(device.name for device in profile_pb.devices.values())
+        self.assertIn("/host:CPU", devices)
+        if config.list_physical_devices("GPU"):
+            self.assertIn("/device:GPU:0", devices)
         events = frozenset(event.name for event in profile_pb.trace_events)
-        self.assertIn('three_times_five', events)
-        self.assertIn('Mul:Mul', events)
+        self.assertIn("three_times_five", events)
+        self.assertIn("Mul:Mul", events)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test.main()
