9d8506867d7bc56b5458f55b72bb2be75d7998a1
Restyled.io <commits@restyled.io>
2020-02-29 10:04:28 +0000

    Restyled by reorder-python-imports

--- tensorflow/compiler/tests/image_ops_test.py
+++ tensorflow/compiler/tests/image_ops_test.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for image ops."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -21,9 +20,8 @@ from __future__ import print_function
 import colorsys
 import math
 
-from absl.testing import parameterized
 import numpy as np
-
+from absl.testing import parameterized
 from six.moves import xrange  # pylint: disable=redefined-builtin
 
 from tensorflow.compiler.tests import xla_test
