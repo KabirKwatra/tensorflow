f2ba4708e442df8d9eaebb6ab2a431b82ebce7fe
Restyled.io <commits@restyled.io>
2020-03-01 20:04:38 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/keras/optimizer_v2/adagrad.py
+++ tensorflow/python/keras/optimizer_v2/adagrad.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 """Adagrad for TensorFlow."""
 from __future__ import absolute_import
 from __future__ import division
