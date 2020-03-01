e8ed910984e67613685405dcaa85dbc8f42ac056
Restyled.io <commits@restyled.io>
2020-03-01 18:04:29 +0000

    Restyled by astyle

--- tensorflow/core/profiler/convert/xplane_to_profile_response.cc
+++ tensorflow/core/profiler/convert/xplane_to_profile_response.cc
@@ -42,23 +42,23 @@ const absl::string_view kOverviewPage = "overview_page";
 const absl::string_view kKernelStats = "kernel_stats";
 
 HardwareType HardwareTypeFromRunEnvironment(const RunEnvironment& run_env) {
-  if (run_env.device_type() == "GPU") return HardwareType::GPU;
-  if (run_env.device_type() == "CPU") return HardwareType::CPU_ONLY;
-  return HardwareType::UNKNOWN_HARDWARE;
+    if (run_env.device_type() == "GPU") return HardwareType::GPU;
+    if (run_env.device_type() == "CPU") return HardwareType::CPU_ONLY;
+    return HardwareType::UNKNOWN_HARDWARE;
 }
 
 template <typename Proto>
 void AddToolData(absl::string_view tool_name, const Proto& tool_output,
                  ProfileResponse* response) {
-  auto* tool_data = response->add_tool_data();
-  tool_data->set_name(string(tool_name));
-  tool_output.SerializeToString(tool_data->mutable_data());
+    auto* tool_data = response->add_tool_data();
+    tool_data->set_name(string(tool_name));
+    tool_output.SerializeToString(tool_data->mutable_data());
 }
 
 // Returns the tool name with extension.
 string ToolName(absl::string_view tool) {
-  if (tool == kTraceViewer) return "trace";
-  return absl::StrCat(tool, ".pb");
+    if (tool == kTraceViewer) return "trace";
+    return absl::StrCat(tool, ".pb");
 }
 
 }  // namespace
@@ -66,39 +66,39 @@ string ToolName(absl::string_view tool) {
 void ConvertXSpaceToProfileResponse(const XSpace& xspace,
                                     const ProfileRequest& req,
                                     ProfileResponse* response) {
-  absl::flat_hash_set<absl::string_view> tools(req.tools().begin(),
-                                               req.tools().end());
-  if (tools.empty()) return;
-  if (tools.contains(kTraceViewer)) {
-    Trace trace;
-    ConvertXSpaceToTraceEvents(xspace, &trace);
-    AddToolData(ToolName(kTraceViewer), trace, response);
-    // Trace viewer is the only tool, skip OpStats conversion.
-    if (tools.size() == 1) return;
-  }
-  OpStats op_stats = ConvertXSpaceToOpStats(xspace);
-  HardwareType hw_type =
-      HardwareTypeFromRunEnvironment(op_stats.run_environment());
-  if (tools.contains(kOverviewPage)) {
-    OverviewPage overview_page_db =
-        ConvertOpStatsToOverviewPage(op_stats, hw_type);
-    AddToolData(ToolName(kOverviewPage), overview_page_db, response);
-    if (tools.contains(kInputPipeline)) {
-      AddToolData(ToolName(kInputPipeline), overview_page_db.input_analysis(),
-                  response);
+    absl::flat_hash_set<absl::string_view> tools(req.tools().begin(),
+            req.tools().end());
+    if (tools.empty()) return;
+    if (tools.contains(kTraceViewer)) {
+        Trace trace;
+        ConvertXSpaceToTraceEvents(xspace, &trace);
+        AddToolData(ToolName(kTraceViewer), trace, response);
+        // Trace viewer is the only tool, skip OpStats conversion.
+        if (tools.size() == 1) return;
     }
-  } else if (tools.contains(kInputPipeline)) {
-    InputPipelineAnalysisResult input_pipeline_analysis =
-        ConvertOpStatsToInputPipelineAnalysis(op_stats, hw_type);
-    AddToolData(ToolName(kInputPipeline), input_pipeline_analysis, response);
-  }
-  if (tools.contains(kTensorflowStats)) {
-    TfStatsDatabase tf_stats_db = ConvertOpStatsToTfStats(op_stats);
-    AddToolData(ToolName(kTensorflowStats), tf_stats_db, response);
-  }
-  if (tools.contains(kKernelStats)) {
-    AddToolData(ToolName(kKernelStats), op_stats.kernel_stats_db(), response);
-  }
+    OpStats op_stats = ConvertXSpaceToOpStats(xspace);
+    HardwareType hw_type =
+        HardwareTypeFromRunEnvironment(op_stats.run_environment());
+    if (tools.contains(kOverviewPage)) {
+        OverviewPage overview_page_db =
+            ConvertOpStatsToOverviewPage(op_stats, hw_type);
+        AddToolData(ToolName(kOverviewPage), overview_page_db, response);
+        if (tools.contains(kInputPipeline)) {
+            AddToolData(ToolName(kInputPipeline), overview_page_db.input_analysis(),
+                        response);
+        }
+    } else if (tools.contains(kInputPipeline)) {
+        InputPipelineAnalysisResult input_pipeline_analysis =
+            ConvertOpStatsToInputPipelineAnalysis(op_stats, hw_type);
+        AddToolData(ToolName(kInputPipeline), input_pipeline_analysis, response);
+    }
+    if (tools.contains(kTensorflowStats)) {
+        TfStatsDatabase tf_stats_db = ConvertOpStatsToTfStats(op_stats);
+        AddToolData(ToolName(kTensorflowStats), tf_stats_db, response);
+    }
+    if (tools.contains(kKernelStats)) {
+        AddToolData(ToolName(kKernelStats), op_stats.kernel_stats_db(), response);
+    }
 }
 
 }  // namespace profiler
--- tensorflow/python/profiler/internal/profiler_wrapper.cc
+++ tensorflow/python/profiler/internal/profiler_wrapper.cc
@@ -32,106 +32,106 @@ namespace py = ::pybind11;
 namespace {
 
 tensorflow::string GetCurrentTimeStampAsString() {
-  return absl::FormatTime("%E4Y-%m-%d_%H:%M:%S", absl::Now(),
-                          absl::LocalTimeZone());
+    return absl::FormatTime("%E4Y-%m-%d_%H:%M:%S", absl::Now(),
+                            absl::LocalTimeZone());
 }
 
 tensorflow::ProfileRequest MakeProfileRequest() {
-  tensorflow::ProfileRequest request;
-  request.add_tools("trace_viewer");
-  request.add_tools("overview_page");
-  request.add_tools("input_pipeline");
-  request.add_tools("kernel_stats");
-  request.add_tools("tensorflow_stats");
-  return request;
+    tensorflow::ProfileRequest request;
+    request.add_tools("trace_viewer");
+    request.add_tools("overview_page");
+    request.add_tools("input_pipeline");
+    request.add_tools("kernel_stats");
+    request.add_tools("tensorflow_stats");
+    return request;
 }
 
 class ProfilerSessionWrapper {
- public:
-  void Start(const char* logdir) {
-    session_ = tensorflow::ProfilerSession::Create();
-    logdir_ = logdir;
-    tensorflow::MaybeRaiseRegisteredFromStatus(session_->Status());
-  }
-
-  py::bytes Stop() {
-    tensorflow::string content;
-    if (session_ != nullptr) {
-      tensorflow::Status status = session_->SerializeToString(&content);
-      session_.reset();
-      tensorflow::MaybeRaiseRegisteredFromStatus(status);
+public:
+    void Start(const char* logdir) {
+        session_ = tensorflow::ProfilerSession::Create();
+        logdir_ = logdir;
+        tensorflow::MaybeRaiseRegisteredFromStatus(session_->Status());
     }
-    // The content is not valid UTF-8, so it must be converted to bytes.
-    return py::bytes(content);
-  }
 
-  void ExportToTensorBoard() {
-    if (!session_ || logdir_.empty()) return;
-    tensorflow::profiler::XSpace xspace;
-    tensorflow::Status status;
-    status = session_->CollectData(&xspace);
-    session_.reset();
-    if (!status.ok()) {
-      tensorflow::MaybeRaiseRegisteredFromStatus(status);
-      return;
+    py::bytes Stop() {
+        tensorflow::string content;
+        if (session_ != nullptr) {
+            tensorflow::Status status = session_->SerializeToString(&content);
+            session_.reset();
+            tensorflow::MaybeRaiseRegisteredFromStatus(status);
+        }
+        // The content is not valid UTF-8, so it must be converted to bytes.
+        return py::bytes(content);
     }
-    tensorflow::ProfileResponse response;
-    tensorflow::profiler::ConvertXSpaceToProfileResponse(
-        xspace, MakeProfileRequest(), &response);
 
-    std::stringstream ss;  // Record LOG messages.
-    status = tensorflow::profiler::SaveTensorboardProfile(
-        logdir_, GetCurrentTimeStampAsString(), tensorflow::port::Hostname(),
-        response, &ss);
-    LOG(INFO) << ss.str();
-    tensorflow::MaybeRaiseRegisteredFromStatus(tensorflow::Status::OK());
-  }
+    void ExportToTensorBoard() {
+        if (!session_ || logdir_.empty()) return;
+        tensorflow::profiler::XSpace xspace;
+        tensorflow::Status status;
+        status = session_->CollectData(&xspace);
+        session_.reset();
+        if (!status.ok()) {
+            tensorflow::MaybeRaiseRegisteredFromStatus(status);
+            return;
+        }
+        tensorflow::ProfileResponse response;
+        tensorflow::profiler::ConvertXSpaceToProfileResponse(
+            xspace, MakeProfileRequest(), &response);
 
- private:
-  std::unique_ptr<tensorflow::ProfilerSession> session_;
-  tensorflow::string logdir_;
+        std::stringstream ss;  // Record LOG messages.
+        status = tensorflow::profiler::SaveTensorboardProfile(
+                     logdir_, GetCurrentTimeStampAsString(), tensorflow::port::Hostname(),
+                     response, &ss);
+        LOG(INFO) << ss.str();
+        tensorflow::MaybeRaiseRegisteredFromStatus(tensorflow::Status::OK());
+    }
+
+private:
+    std::unique_ptr<tensorflow::ProfilerSession> session_;
+    tensorflow::string logdir_;
 };
 
 }  // namespace
 
 PYBIND11_MODULE(_pywrap_profiler, m) {
-  py::class_<ProfilerSessionWrapper> profiler_session_class(m,
-                                                            "ProfilerSession");
-  profiler_session_class.def(py::init<>())
-      .def("start", &ProfilerSessionWrapper::Start)
-      .def("stop", &ProfilerSessionWrapper::Stop)
-      .def("export_to_tb", &ProfilerSessionWrapper::ExportToTensorBoard);
+    py::class_<ProfilerSessionWrapper> profiler_session_class(m,
+            "ProfilerSession");
+    profiler_session_class.def(py::init<>())
+    .def("start", &ProfilerSessionWrapper::Start)
+    .def("stop", &ProfilerSessionWrapper::Stop)
+    .def("export_to_tb", &ProfilerSessionWrapper::ExportToTensorBoard);
 
-  m.def("start_server", [](int port) {
-    auto profiler_server = absl::make_unique<tensorflow::ProfilerServer>();
-    profiler_server->StartProfilerServer(port);
-    // Intentionally release profiler server. Should transfer ownership to
-    // caller instead.
-    profiler_server.release();
-  });
+    m.def("start_server", [](int port) {
+        auto profiler_server = absl::make_unique<tensorflow::ProfilerServer>();
+        profiler_server->StartProfilerServer(port);
+        // Intentionally release profiler server. Should transfer ownership to
+        // caller instead.
+        profiler_server.release();
+    });
 
-  m.def("trace", [](const char* service_addr, const char* logdir,
-                    const char* worker_list, bool include_dataset_ops,
-                    int duration_ms, int num_tracing_attempts) {
-    tensorflow::Status status =
-        tensorflow::profiler::ValidateHostPortPair(service_addr);
-    tensorflow::MaybeRaiseRegisteredFromStatus(status);
-    status = tensorflow::profiler::Trace(service_addr, logdir, worker_list,
-                                         include_dataset_ops, duration_ms,
-                                         num_tracing_attempts);
-    tensorflow::MaybeRaiseRegisteredFromStatus(status);
-  });
+    m.def("trace", [](const char* service_addr, const char* logdir,
+                      const char* worker_list, bool include_dataset_ops,
+    int duration_ms, int num_tracing_attempts) {
+        tensorflow::Status status =
+            tensorflow::profiler::ValidateHostPortPair(service_addr);
+        tensorflow::MaybeRaiseRegisteredFromStatus(status);
+        status = tensorflow::profiler::Trace(service_addr, logdir, worker_list,
+                                             include_dataset_ops, duration_ms,
+                                             num_tracing_attempts);
+        tensorflow::MaybeRaiseRegisteredFromStatus(status);
+    });
 
-  m.def("monitor", [](const char* service_addr, int duration_ms,
-                      int monitoring_level, bool display_timestamp) {
-    tensorflow::Status status =
-        tensorflow::profiler::ValidateHostPortPair(service_addr);
-    tensorflow::MaybeRaiseRegisteredFromStatus(status);
-    tensorflow::string content;
-    status = tensorflow::profiler::Monitor(service_addr, duration_ms,
-                                           monitoring_level, display_timestamp,
-                                           &content);
-    tensorflow::MaybeRaiseRegisteredFromStatus(status);
-    return content;
-  });
+    m.def("monitor", [](const char* service_addr, int duration_ms,
+    int monitoring_level, bool display_timestamp) {
+        tensorflow::Status status =
+            tensorflow::profiler::ValidateHostPortPair(service_addr);
+        tensorflow::MaybeRaiseRegisteredFromStatus(status);
+        tensorflow::string content;
+        status = tensorflow::profiler::Monitor(service_addr, duration_ms,
+                                               monitoring_level, display_timestamp,
+                                               &content);
+        tensorflow::MaybeRaiseRegisteredFromStatus(status);
+        return content;
+    });
 };
