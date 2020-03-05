f3b34270bbd7998f6d49e0f5173395bef7912e7a
Restyled.io <commits@restyled.io>
2020-03-05 20:04:54 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/keras/distribute/multi_worker_callback_tf2_test.py
+++ tensorflow/python/keras/distribute/multi_worker_callback_tf2_test.py
@@ -13,10 +13,10 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for Keras callbacks in multi-worker training with TF2."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 import os
 
 from absl.testing import parameterized
