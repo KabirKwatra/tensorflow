982fb08f5f5051e825c38ad38a49abc032808d81
Restyled.io <commits@restyled.io>
2020-03-05 02:04:39 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/keras/layers/preprocessing/image_preprocessing_test.py
+++ tensorflow/python/keras/layers/preprocessing/image_preprocessing_test.py
@@ -13,13 +13,12 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for image preprocessing layers."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl.testing import parameterized
 import numpy as np
+from absl.testing import parameterized
 
 from tensorflow.python.compat import compat
 from tensorflow.python.framework import errors
