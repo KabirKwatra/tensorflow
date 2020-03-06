1f6ab8868cc96286c41f798b9cc6302d248b0a50
Restyled.io <commits@restyled.io>
2020-03-06 16:04:35 +0000

    Restyled by reorder-python-imports

--- tensorflow/compiler/tests/searchsorted_op_test.py
+++ tensorflow/compiler/tests/searchsorted_op_test.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 """Test for XLA implementation of tf.searchsorted."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
