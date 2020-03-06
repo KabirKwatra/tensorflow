52bb7ab4dbc197e0016494b46ff8b8559792b01a
Restyled.io <commits@restyled.io>
2020-03-06 12:04:28 +0000

    Restyled by astyle

--- tensorflow/compiler/mlir/xla/transforms/lhlo_copy_removal.cc
+++ tensorflow/compiler/mlir/xla/transforms/lhlo_copy_removal.cc
@@ -31,71 +31,71 @@ namespace {
 // argument and the buffer is freed. Note that this pass only works in regions
 // with a single block.
 struct LhloCopyRemoval : mlir::OperationPass<LhloCopyRemoval> {
-  void runOnOperation() override {
-    llvm::SmallVector<mlir::Operation*, 2> eraseList;
-    auto operation = getOperation();
-    operation->walk([&](mlir::xla_lhlo::CopyOp copyOp) {
-      // If this region contains more than one block, then ignore this copy
-      // operation.
-      if (copyOp.getParentRegion()->getBlocks().size() > 1) {
-        return;
-      }
+    void runOnOperation() override {
+        llvm::SmallVector<mlir::Operation*, 2> eraseList;
+        auto operation = getOperation();
+        operation->walk([&](mlir::xla_lhlo::CopyOp copyOp) {
+            // If this region contains more than one block, then ignore this copy
+            // operation.
+            if (copyOp.getParentRegion()->getBlocks().size() > 1) {
+                return;
+            }
 
-      mlir::Value fromOperand = copyOp.operand();
-      mlir::Value toOperand = copyOp.output();
+            mlir::Value fromOperand = copyOp.operand();
+            mlir::Value toOperand = copyOp.output();
 
-      // If the fromOperand value is a block argument or the toOperand
-      // value is not a block argument, then ignore this copy operation.
-      if (!fromOperand.getDefiningOp() || toOperand.getDefiningOp()) {
-        return;
-      }
+            // If the fromOperand value is a block argument or the toOperand
+            // value is not a block argument, then ignore this copy operation.
+            if (!fromOperand.getDefiningOp() || toOperand.getDefiningOp()) {
+                return;
+            }
 
-      // The copy operation removal is illegal if there is at least a single use
-      // of toOperand value that lies between the first use of fromOperand value
-      // and the copy operation.
-      auto fromOperandUsers = fromOperand.getUsers();
-      auto firstUser = *fromOperandUsers.begin();
-      for (auto op : fromOperandUsers) {
-        if (op->isBeforeInBlock(firstUser)) firstUser = op;
-      }
-      for (auto op : toOperand.getUsers()) {
-        if (op->isBeforeInBlock(copyOp) && firstUser->isBeforeInBlock(op)) {
-          return;
+            // The copy operation removal is illegal if there is at least a single use
+            // of toOperand value that lies between the first use of fromOperand value
+            // and the copy operation.
+            auto fromOperandUsers = fromOperand.getUsers();
+            auto firstUser = *fromOperandUsers.begin();
+            for (auto op : fromOperandUsers) {
+                if (op->isBeforeInBlock(firstUser)) firstUser = op;
+            }
+            for (auto op : toOperand.getUsers()) {
+                if (op->isBeforeInBlock(copyOp) && firstUser->isBeforeInBlock(op)) {
+                    return;
+                }
+            }
+
+            // TODO(DFKI): Use live variable analysis to solve aliasing issues among
+            // block arguments.
+
+            // Remove the associated alloc operation.
+            auto allocOp = fromOperand.getDefiningOp();
+            eraseList.push_back(allocOp);
+
+            // Iterate over all uses of the fromOperand to find the associated
+            // deallocOp (if any).
+            for (auto op : fromOperandUsers) {
+                if (isa<mlir::DeallocOp>(op)) {
+                    eraseList.push_back(op);
+                    break;
+                }
+            }
+
+            // Replace all uses of the fromOperand with the toOperand. This rewires
+            // all references pointing to the original alloc operation to the new
+            // target operation in order to safely remove the copy op.
+            fromOperand.replaceAllUsesWith(toOperand);
+            copyOp.erase();
+        });
+        for (auto op : eraseList) {
+            op->erase();
         }
-      }
-
-      // TODO(DFKI): Use live variable analysis to solve aliasing issues among
-      // block arguments.
-
-      // Remove the associated alloc operation.
-      auto allocOp = fromOperand.getDefiningOp();
-      eraseList.push_back(allocOp);
-
-      // Iterate over all uses of the fromOperand to find the associated
-      // deallocOp (if any).
-      for (auto op : fromOperandUsers) {
-        if (isa<mlir::DeallocOp>(op)) {
-          eraseList.push_back(op);
-          break;
-        }
-      }
-
-      // Replace all uses of the fromOperand with the toOperand. This rewires
-      // all references pointing to the original alloc operation to the new
-      // target operation in order to safely remove the copy op.
-      fromOperand.replaceAllUsesWith(toOperand);
-      copyOp.erase();
-    });
-    for (auto op : eraseList) {
-      op->erase();
-    }
-  };
+    };
 };
 
 }  // namespace
 
 std::unique_ptr<Pass> createLhloCopyRemovalPass() {
-  return absl::make_unique<LhloCopyRemoval>();
+    return absl::make_unique<LhloCopyRemoval>();
 }
 
 static PassRegistration<LhloCopyRemoval> copy_removal_pass(
