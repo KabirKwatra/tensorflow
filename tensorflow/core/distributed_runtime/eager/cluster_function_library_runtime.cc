840fc132c0efbfeb76a2d221bce9694662cad360
Restyled.io <commits@restyled.io>
2020-03-07 00:04:29 +0000

    Restyled by clang-format

--- tensorflow/core/distributed_runtime/eager/cluster_function_library_runtime.cc
+++ tensorflow/core/distributed_runtime/eager/cluster_function_library_runtime.cc
@@ -33,13 +33,13 @@ namespace eager {
 namespace {
 void StripDefaultAttributesInRegisterFunctionOp(
     RegisterFunctionOp* register_function) {
-    StripDefaultAttributes(
-        *OpRegistry::Global(),
-        register_function->mutable_function_def()->mutable_node_def());
-    for (auto& function :
-            *register_function->mutable_library()->mutable_function()) {
-        StripDefaultAttributes(*OpRegistry::Global(), function.mutable_node_def());
-    }
+  StripDefaultAttributes(
+      *OpRegistry::Global(),
+      register_function->mutable_function_def()->mutable_node_def());
+  for (auto& function :
+       *register_function->mutable_library()->mutable_function()) {
+    StripDefaultAttributes(*OpRegistry::Global(), function.mutable_node_def());
+  }
 }
 }  // namespace
 
@@ -48,90 +48,90 @@ void EagerClusterFunctionLibraryRuntime::Instantiate(
     AttrSlice attrs, const FunctionLibraryRuntime::InstantiateOptions& options,
     FunctionLibraryRuntime::LocalHandle* handle,
     FunctionLibraryRuntime::DoneCallback done) {
-    auto target = options.target;
-    auto released_op = std::make_unique<EagerOperation>(ctx_);
-    Status s =
-        released_op->Reset(function_name.c_str(), target.c_str(), true, nullptr);
-    if (!s.ok()) {
-        done(s);
-        return;
-    }
-    if (!released_op->is_function()) {
-        done(errors::Internal(function_name, " is not a function."));
-        return;
-    }
+  auto target = options.target;
+  auto released_op = std::make_unique<EagerOperation>(ctx_);
+  Status s =
+      released_op->Reset(function_name.c_str(), target.c_str(), true, nullptr);
+  if (!s.ok()) {
+    done(s);
+    return;
+  }
+  if (!released_op->is_function()) {
+    done(errors::Internal(function_name, " is not a function."));
+    return;
+  }
 
-    VLOG(1) << "CFLR::Instantiate: " << function_name << " on " << target
-            << " (this: " << this << ")";
-    core::RefCountPtr<eager::EagerClient> eager_client;
-    Device* device;
-    s = ctx_->FindDeviceFromName(target.c_str(), &device);
-    if (!s.ok()) {
-        done(s);
-        return;
-    }
-    s = ctx_->GetClient(device, &eager_client);
-    if (!s.ok()) {
-        done(s);
-        return;
-    }
+  VLOG(1) << "CFLR::Instantiate: " << function_name << " on " << target
+          << " (this: " << this << ")";
+  core::RefCountPtr<eager::EagerClient> eager_client;
+  Device* device;
+  s = ctx_->FindDeviceFromName(target.c_str(), &device);
+  if (!s.ok()) {
+    done(s);
+    return;
+  }
+  s = ctx_->GetClient(device, &eager_client);
+  if (!s.ok()) {
+    done(s);
+    return;
+  }
 
-    if (eager_client == nullptr) {
-        done(errors::InvalidArgument("Could not find eager client for target: ",
-                                     target));
-        return;
-    }
+  if (eager_client == nullptr) {
+    done(errors::InvalidArgument("Could not find eager client for target: ",
+                                 target));
+    return;
+  }
 
-    const FunctionLibraryDefinition& func_lib_def =
-        options.lib_def ? *options.lib_def : lib_def;
+  const FunctionLibraryDefinition& func_lib_def =
+      options.lib_def ? *options.lib_def : lib_def;
 
-    EnqueueRequest* request = new EnqueueRequest;
-    EnqueueResponse* response = new EnqueueResponse;
+  EnqueueRequest* request = new EnqueueRequest;
+  EnqueueResponse* response = new EnqueueResponse;
 
-    request->set_context_id(context_id_);
+  request->set_context_id(context_id_);
 
-    RegisterFunctionOp* register_function =
-        request->add_queue()->mutable_register_function();
-    *register_function->mutable_function_def() =
-        *func_lib_def.Find(function_name);
-    register_function->set_is_component_function(true);
-    *register_function->mutable_library() =
-        func_lib_def.ReachableDefinitions(register_function->function_def())
-        .ToProto();
-    StripDefaultAttributesInRegisterFunctionOp(register_function);
+  RegisterFunctionOp* register_function =
+      request->add_queue()->mutable_register_function();
+  *register_function->mutable_function_def() =
+      *func_lib_def.Find(function_name);
+  register_function->set_is_component_function(true);
+  *register_function->mutable_library() =
+      func_lib_def.ReachableDefinitions(register_function->function_def())
+          .ToProto();
+  StripDefaultAttributesInRegisterFunctionOp(register_function);
 
-    eager_client->EnqueueAsync(
-        request, response,
-        [this, request, response, handle, released_op = released_op.release(),
-          target, eager_client = eager_client.get(), done](const Status& s) {
+  eager_client->EnqueueAsync(
+      request, response,
+      [this, request, response, handle, released_op = released_op.release(),
+       target, eager_client = eager_client.get(), done](const Status& s) {
         {
-            mutex_lock l(mu_);
-            *handle = function_data_.size();
-            function_data_.emplace_back(target, eager_client,
-                                        absl::WrapUnique(released_op));
+          mutex_lock l(mu_);
+          *handle = function_data_.size();
+          function_data_.emplace_back(target, eager_client,
+                                      absl::WrapUnique(released_op));
         }
         done(s);
         delete request;
         delete response;
-    });
+      });
 }
 
 void EagerClusterFunctionLibraryRuntime::Run(
     const FunctionLibraryRuntime::Options& opts,
     FunctionLibraryRuntime::LocalHandle handle, gtl::ArraySlice<Tensor> args,
     std::vector<Tensor>* rets, FunctionLibraryRuntime::DoneCallback done) {
-    if (args.empty() && rets->empty()) {
-        FunctionLibraryRuntime::Options opts_copy = opts;
-        opts_copy.op_id = ctx_->RemoteMgr()->NextOpId();
-        Run(opts_copy, handle, /*args=*/nullptr, std::move(done));
-    } else {
-        // TODO(b/150963957): Support remote inputs and outputs which are passed as
-        // Tensors.
-        done(errors::Unimplemented(
-                 "Not implemented. Users could set the input devices and output devices "
-                 "in FunctionLibraryRuntime::Options to the default multi-device "
-                 "function device as a workaround."));
-    }
+  if (args.empty() && rets->empty()) {
+    FunctionLibraryRuntime::Options opts_copy = opts;
+    opts_copy.op_id = ctx_->RemoteMgr()->NextOpId();
+    Run(opts_copy, handle, /*args=*/nullptr, std::move(done));
+  } else {
+    // TODO(b/150963957): Support remote inputs and outputs which are passed as
+    // Tensors.
+    done(errors::Unimplemented(
+        "Not implemented. Users could set the input devices and output devices "
+        "in FunctionLibraryRuntime::Options to the default multi-device "
+        "function device as a workaround."));
+  }
 }
 
 void EagerClusterFunctionLibraryRuntime::Run(
@@ -139,112 +139,112 @@ void EagerClusterFunctionLibraryRuntime::Run(
     FunctionLibraryRuntime::LocalHandle handle,
     std::vector<eager::RemoteTensorHandle>* args,
     FunctionLibraryRuntime::DoneCallback done) {
-    FunctionData* function_data = nullptr;
-    {
-        mutex_lock l(mu_);
-        DCHECK_LE(handle, function_data_.size());
-        function_data = &function_data_[handle];
+  FunctionData* function_data = nullptr;
+  {
+    mutex_lock l(mu_);
+    DCHECK_LE(handle, function_data_.size());
+    function_data = &function_data_[handle];
+  }
+
+  EagerClient* eager_client = function_data->eager_client.get();
+  if (eager_client == nullptr) {
+    done(errors::Internal("Could not find eager client"));
+    return;
+  }
+
+  Device* device;
+  Status s = ctx_->FindDeviceFromName(function_data->target.c_str(), &device);
+  if (!s.ok()) {
+    done(errors::Internal("Failed to get device"));
+    return;
+  }
+
+  EagerOperation* op = function_data->op.get();
+
+  if (!opts.op_id.has_value()) {
+    done(
+        errors::Internal("op_id is not set for remote function: ", op->Name()));
+  }
+
+  eager::EnqueueRequest* request = new eager::EnqueueRequest;
+  request->set_context_id(context_id_);
+  eager::Operation* remote_op = request->add_queue()->mutable_operation();
+  if (args) {
+    for (size_t i = 0; i < args->size(); ++i) {
+      remote_op->add_inputs()->Swap(&(*args)[i]);
     }
+  }
+  // The remote component function should use the same op_id as its parent
+  // multi-device function's in order to get the global unique op_id generated
+  // by the master context.
+  remote_op->set_id(opts.op_id.value());
+  remote_op->set_is_function(true);
+  remote_op->set_is_component_function(true);
+  remote_op->set_func_step_id(opts.step_id);
+  remote_op->set_name(op->Name());
+  op->Attrs().FillAttrValueMap(remote_op->mutable_attrs());
+  remote_op->set_device(function_data->target);
 
-    EagerClient* eager_client = function_data->eager_client.get();
-    if (eager_client == nullptr) {
-        done(errors::Internal("Could not find eager client"));
-        return;
-    }
+  for (auto handle : op->Inputs()) {
+    handle->Ref();
+  }
 
-    Device* device;
-    Status s = ctx_->FindDeviceFromName(function_data->target.c_str(), &device);
-    if (!s.ok()) {
-        done(errors::Internal("Failed to get device"));
-        return;
-    }
-
-    EagerOperation* op = function_data->op.get();
-
-    if (!opts.op_id.has_value()) {
-        done(
-            errors::Internal("op_id is not set for remote function: ", op->Name()));
-    }
-
-    eager::EnqueueRequest* request = new eager::EnqueueRequest;
-    request->set_context_id(context_id_);
-    eager::Operation* remote_op = request->add_queue()->mutable_operation();
-    if (args) {
-        for (size_t i = 0; i < args->size(); ++i) {
-            remote_op->add_inputs()->Swap(&(*args)[i]);
-        }
-    }
-    // The remote component function should use the same op_id as its parent
-    // multi-device function's in order to get the global unique op_id generated
-    // by the master context.
-    remote_op->set_id(opts.op_id.value());
-    remote_op->set_is_function(true);
-    remote_op->set_is_component_function(true);
-    remote_op->set_func_step_id(opts.step_id);
-    remote_op->set_name(op->Name());
-    op->Attrs().FillAttrValueMap(remote_op->mutable_attrs());
-    remote_op->set_device(function_data->target);
-
-    for (auto handle : op->Inputs()) {
-        handle->Ref();
-    }
-
-    // StreamingEnqueueAsync may introduce a deadlock. When streaming RPC is
-    // disabled, Run() returns when the remote function execution completes, which
-    // might be blocked by a non-enqueued function execution.
-    EnqueueResponse* response = new EnqueueResponse;
-    eager_client->EnqueueAsync(request, response,
-    [op, request, response, done](const Status& s) {
-        for (auto handle : op->Inputs()) {
-            handle->Unref();
-        }
-        done(s);
-        delete request;
-        delete response;
-    });
+  // StreamingEnqueueAsync may introduce a deadlock. When streaming RPC is
+  // disabled, Run() returns when the remote function execution completes, which
+  // might be blocked by a non-enqueued function execution.
+  EnqueueResponse* response = new EnqueueResponse;
+  eager_client->EnqueueAsync(request, response,
+                             [op, request, response, done](const Status& s) {
+                               for (auto handle : op->Inputs()) {
+                                 handle->Unref();
+                               }
+                               done(s);
+                               delete request;
+                               delete response;
+                             });
 }
 
 void EagerClusterFunctionLibraryRuntime::CleanUp(
     uint64 step_id, FunctionLibraryRuntime::LocalHandle handle,
     FunctionLibraryRuntime::DoneCallback done) {
-    FunctionData* function_data = nullptr;
-    {
-        mutex_lock l(mu_);
-        DCHECK_LE(handle, function_data_.size());
-        function_data = &function_data_[handle];
-    }
+  FunctionData* function_data = nullptr;
+  {
+    mutex_lock l(mu_);
+    DCHECK_LE(handle, function_data_.size());
+    function_data = &function_data_[handle];
+  }
 
-    EagerClient* eager_client = function_data->eager_client.get();
-    if (eager_client == nullptr) {
-        done(errors::Internal("Could not find eager client"));
-        return;
-    }
+  EagerClient* eager_client = function_data->eager_client.get();
+  if (eager_client == nullptr) {
+    done(errors::Internal("Could not find eager client"));
+    return;
+  }
 
-    eager::EnqueueRequest* request = new eager::EnqueueRequest;
-    EnqueueResponse* response = new EnqueueResponse;
-    request->set_context_id(context_id_);
-    CleanupFunctionOp* cleanup_function =
-        request->add_queue()->mutable_cleanup_function();
-    cleanup_function->set_step_id(step_id);
-    // StreamingEnqueueAsync could be blocking when streaming RPC is disabled.
-    // CleanUp() needs to be non-blocking since it would be invoked inside the
-    // enqueue done callback of Run(). So we don't use StreamingEnqueueAsync here.
-    eager_client->EnqueueAsync(request, response,
-    [request, response, done](const Status& status) {
-        done(status);
-        delete request;
-        delete response;
-    });
+  eager::EnqueueRequest* request = new eager::EnqueueRequest;
+  EnqueueResponse* response = new EnqueueResponse;
+  request->set_context_id(context_id_);
+  CleanupFunctionOp* cleanup_function =
+      request->add_queue()->mutable_cleanup_function();
+  cleanup_function->set_step_id(step_id);
+  // StreamingEnqueueAsync could be blocking when streaming RPC is disabled.
+  // CleanUp() needs to be non-blocking since it would be invoked inside the
+  // enqueue done callback of Run(). So we don't use StreamingEnqueueAsync here.
+  eager_client->EnqueueAsync(request, response,
+                             [request, response, done](const Status& status) {
+                               done(status);
+                               delete request;
+                               delete response;
+                             });
 }
 
 DistributedFunctionLibraryRuntime* CreateClusterFLR(
     const uint64 context_id, EagerContext* ctx, WorkerSession* worker_session) {
-    if (ctx->LazyCopyFunctionRemoteInputs()) {
-        return new EagerClusterFunctionLibraryRuntime(
-                   context_id, ctx, worker_session->remote_device_mgr());
-    } else {
-        return worker_session->cluster_flr();
-    }
+  if (ctx->LazyCopyFunctionRemoteInputs()) {
+    return new EagerClusterFunctionLibraryRuntime(
+        context_id, ctx, worker_session->remote_device_mgr());
+  } else {
+    return worker_session->cluster_flr();
+  }
 }
 
 }  // namespace eager
