ab748cc1abc239351cdb529fd79932da1ba9de55
Restyled.io <commits@restyled.io>
2020-03-07 02:04:46 +0000

    Restyled by shellharden

--- tensorflow/lite/lib_package/create_ios_frameworks.sh
+++ tensorflow/lite/lib_package/create_ios_frameworks.sh
@@ -41,10 +41,10 @@ shift $((OPTIND - 1))
 readonly USE_GPU_DELEGATE
 readonly FRAMEWORK_NAME
 
-if [ $USE_GPU_DELEGATE == "true" ] ; then
+if [ "$USE_GPU_DELEGATE" == "true" ] ; then
   for filename in metal_delegate.h libmetal_delegate.a ; do
     if [[ ! -f "${TFLITE_DIR}/delegates/gpu/${filename}" ]] ; then
-      echo "File ${TFLITE_DIR}/delegates/gpu/${filename} doesn't exist."
+      echo "File $TFLITE_DIR/delegates/gpu/$filename doesn't exist."
       echo "It's required for building TFLite Framework with GPU. Aborting."
       exit 1
     fi
@@ -52,16 +52,16 @@ if [ $USE_GPU_DELEGATE == "true" ] ; then
 fi
 
 TMP_DIR=$(mktemp -d)
-echo "Package dir: " $TMP_DIR
+echo "Package dir: " "$TMP_DIR"
 FW_DIR=$TMP_DIR/tensorflow_lite_ios_frameworks
 FW_DIR_TFLITE=$FW_DIR/$FRAMEWORK_NAME.framework
 FW_DIR_TFLITE_HDRS=$FW_DIR_TFLITE/Headers
 
 echo "Creating target Headers directories"
-mkdir -p $FW_DIR_TFLITE_HDRS
+mkdir -p "$FW_DIR_TFLITE_HDRS"
 
 echo "Headers, populating: TensorFlow Lite"
-cd $TFLITE_DIR/../..
+cd "$TFLITE_DIR"/../..
 
 find tensorflow/lite -name '*.h' \
     -not -path 'tensorflow/lite/tools/*' \
@@ -70,51 +70,51 @@ find tensorflow/lite -name '*.h' \
     -not -path 'tensorflow/lite/toco/*' \
     -not -path 'tensorflow/lite/nnapi/*' \
     -not -path 'tensorflow/lite/java/*' \
-    | tar -cf $FW_DIR_TFLITE_HDRS/tmp.tar -T -
-cd $FW_DIR_TFLITE_HDRS
+    | tar -cf "$FW_DIR_TFLITE_HDRS"/tmp.tar -T -
+cd "$FW_DIR_TFLITE_HDRS"
 tar xf tmp.tar
 rm -f tmp.tar
 
 echo "Headers, populating: Flatbuffer"
-cd $TFLITE_DIR/tools/make/downloads/flatbuffers/include/
-find . -name '*.h' | tar -cf $FW_DIR_TFLITE_HDRS/tmp.tar -T -
-cd $FW_DIR_TFLITE_HDRS
+cd "$TFLITE_DIR"/tools/make/downloads/flatbuffers/include/
+find . -name '*.h' | tar -cf "$FW_DIR_TFLITE_HDRS"/tmp.tar -T -
+cd "$FW_DIR_TFLITE_HDRS"
 tar xf tmp.tar
 rm -f tmp.tar
 
-cd $TFLITE_DIR/../..
+cd "$TFLITE_DIR"/../..
 echo "Generate LICENSE files and copy to target"
 bazel build //tensorflow/tools/lib_package:clicenses_generate
-cp $TFLITE_DIR/../../LICENSE $FW_DIR_TFLITE
-cp $TFLITE_DIR/../../bazel-bin/tensorflow/tools/lib_package/THIRD_PARTY_TF_C_LICENSES \
-   $FW_DIR_TFLITE
+cp "$TFLITE_DIR"/../../LICENSE "$FW_DIR_TFLITE"
+cp "$TFLITE_DIR"/../../bazel-bin/tensorflow/tools/lib_package/THIRD_PARTY_TF_C_LICENSES \
+   "$FW_DIR_TFLITE"
 
 echo "Copying static libraries"
 # Note: There must be a static library with the same name
 # as the framework name.
-cp $TFLITE_DIR/tools/make/gen/lib/libtensorflow-lite.a \
-    $FW_DIR_TFLITE/$FRAMEWORK_NAME
-if [ $USE_GPU_DELEGATE == "true" ] ; then
-  cp "${TFLITE_DIR}/delegates/gpu/libmetal_delegate.a" \
-      $FW_DIR_TFLITE/libmetal_delegate.a
+cp "$TFLITE_DIR"/tools/make/gen/lib/libtensorflow-lite.a \
+    "$FW_DIR_TFLITE/$FRAMEWORK_NAME"
+if [ "$USE_GPU_DELEGATE" == "true" ] ; then
+  cp "$TFLITE_DIR/delegates/gpu/libmetal_delegate.a" \
+      "$FW_DIR_TFLITE"/libmetal_delegate.a
 fi
 
 # This is required, otherwise they interfere with the documentation of the
 # pod at cocoapods.org.
 echo "Remove all README files"
-cd $FW_DIR_TFLITE_HDRS
+cd "$FW_DIR_TFLITE_HDRS"
 find . -type f -name README\* -exec rm -f {} \;
 find . -type f -name readme\* -exec rm -f {} \;
 
 TARGET_GEN_LOCATION="$TFLITE_DIR/gen/ios_frameworks"
-echo "Moving results to target: " $TARGET_GEN_LOCATION
-cd $FW_DIR
-zip -q -r $FRAMEWORK_NAME.framework.zip $FRAMEWORK_NAME.framework -x .DS_Store
-rm -rf $TARGET_GEN_LOCATION
-mkdir -p $TARGET_GEN_LOCATION
-cp -r $FRAMEWORK_NAME.framework.zip $TARGET_GEN_LOCATION
+echo "Moving results to target: " "$TARGET_GEN_LOCATION"
+cd "$FW_DIR"
+zip -q -r "$FRAMEWORK_NAME".framework.zip "$FRAMEWORK_NAME".framework -x .DS_Store
+rm -rf "$TARGET_GEN_LOCATION"
+mkdir -p "$TARGET_GEN_LOCATION"
+cp -r "$FRAMEWORK_NAME".framework.zip "$TARGET_GEN_LOCATION"
 
 echo "Cleaning up"
-rm -rf $TMP_DIR
+rm -rf "$TMP_DIR"
 
 echo "Finished"
