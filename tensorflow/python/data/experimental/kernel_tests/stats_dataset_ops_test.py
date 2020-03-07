e16e96cb906d7aba17e8aec490746f2a5eb36119
Restyled.io <commits@restyled.io>
2020-03-07 22:04:30 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/data/experimental/kernel_tests/stats_dataset_ops_test.py
+++ tensorflow/python/data/experimental/kernel_tests/stats_dataset_ops_test.py
@@ -17,8 +17,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl.testing import parameterized
 import numpy as np
+from absl.testing import parameterized
 
 from tensorflow.python.data.experimental.kernel_tests import (
     reader_dataset_ops_test_base,
