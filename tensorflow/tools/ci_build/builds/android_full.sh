78f5bac7acf483bca7f1ddb334fc4d006e8a3891
Restyled.io <commits@restyled.io>
2020-03-08 10:04:31 +0000

    Restyled by shellharden

--- tensorflow/tools/ci_build/builds/android_full.sh
+++ tensorflow/tools/ci_build/builds/android_full.sh
@@ -18,29 +18,29 @@ set -e
 
 copy_lib() {
   FILE=$1
-  TARGET_DIR=${OUT_DIR}/native/$(basename $FILE)/${CPU}
-  mkdir -p ${TARGET_DIR}
-  echo "Copying ${FILE} to ${TARGET_DIR}"
-  cp ${FILE} ${TARGET_DIR}
+  TARGET_DIR=$OUT_DIR/native/$(basename "$FILE")/$CPU
+  mkdir -p "$TARGET_DIR"
+  echo "Copying $FILE to $TARGET_DIR"
+  cp "$FILE" "$TARGET_DIR"
 }
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${SCRIPT_DIR}/builds_common.sh"
+source "$SCRIPT_DIR/builds_common.sh"
 configure_android_workspace
 
 CPUS=armeabi-v7a,arm64-v8a,x86,x86_64
 
-OUT_DIR="$(pwd)/out/"
-AAR_LIB_TMP="$(pwd)/aar_libs"
+OUT_DIR="$PWD/out/"
+AAR_LIB_TMP="$PWD/aar_libs"
 
-rm -rf ${OUT_DIR}
-rm -rf ${AAR_LIB_TMP}
+rm -rf "$OUT_DIR"
+rm -rf "$AAR_LIB_TMP"
 
 # Build all relevant native libraries for each architecture.
-for CPU in ${CPUS//,/ }
+for CPU in "${CPUS//,/ }"
 do
-    echo "========== Building native libs for Android ${CPU} =========="
-    bazel build --config=monolithic --cpu=${CPU} \
+    echo "========== Building native libs for Android $CPU =========="
+    bazel build --config=monolithic --cpu="$CPU" \
         --compilation_mode=opt --cxxopt=-std=c++14 \
         --crosstool_top=//external:android/crosstool \
         --host_crosstool_top=@bazel_tools//tools/cpp:toolchain \
@@ -53,8 +53,8 @@ do
     copy_lib bazel-bin/tensorflow/examples/android/libtensorflow_demo.so
     copy_lib bazel-bin/tensorflow/tools/benchmark/benchmark_model
 
-    mkdir -p ${AAR_LIB_TMP}/jni/${CPU}
-    cp bazel-bin/tensorflow/tools/android/inference_interface/libtensorflow_inference.so ${AAR_LIB_TMP}/jni/${CPU}
+    mkdir -p "$AAR_LIB_TMP/jni/$CPU"
+    cp bazel-bin/tensorflow/tools/android/inference_interface/libtensorflow_inference.so "$AAR_LIB_TMP/jni/$CPU"
 done
 
 # Build Jar and also demo containing native libs for all architectures.
@@ -62,7 +62,7 @@ done
 # in assets/ dir (see https://github.com/bazelbuild/bazel/issues/2334)
 # TODO(gunan): remove extra flags once sandboxing is enabled for all builds.
 echo "========== Building TensorFlow Android Jar and Demo =========="
-bazel --bazelrc=/dev/null build --config=monolithic --fat_apk_cpu=${CPUS} \
+bazel --bazelrc=/dev/null build --config=monolithic --fat_apk_cpu="$CPUS" \
     --compilation_mode=opt --cxxopt=-std=c++14 \
     --host_crosstool_top=@bazel_tools//tools/cpp:toolchain \
     --spawn_strategy=sandboxed --genrule_strategy=sandboxed \
@@ -70,20 +70,20 @@ bazel --bazelrc=/dev/null build --config=monolithic --fat_apk_cpu=${CPUS} \
     //tensorflow/tools/android/inference_interface:android_tensorflow_inference_java.aar \
     //tensorflow/examples/android:tensorflow_demo
 
-echo "Copying demo, AAR and Jar to ${OUT_DIR}"
+echo "Copying demo, AAR and Jar to $OUT_DIR"
 cp bazel-bin/tensorflow/examples/android/tensorflow_demo.apk \
-    bazel-bin/tensorflow/tools/android/inference_interface/libandroid_tensorflow_inference_java.jar ${OUT_DIR}
+    bazel-bin/tensorflow/tools/android/inference_interface/libandroid_tensorflow_inference_java.jar "$OUT_DIR"
 
 cp bazel-bin/tensorflow/tools/android/inference_interface/android_tensorflow_inference_java.aar \
-   ${OUT_DIR}/tensorflow.aar
+   "$OUT_DIR"/tensorflow.aar
 
 # TODO(andrewharp): build native libs into AAR directly once
 # https://github.com/bazelbuild/bazel/issues/348 is resolved.
 echo "Adding native libs to AAR"
-chmod +w ${OUT_DIR}/tensorflow.aar
-pushd ${AAR_LIB_TMP}
-zip -ur ${OUT_DIR}/tensorflow.aar $(find jni -name *.so)
+chmod +w "$OUT_DIR"/tensorflow.aar
+pushd "$AAR_LIB_TMP"
+zip -ur "$OUT_DIR"/tensorflow.aar "$(find jni -name *.so)"
 popd
-rm -rf ${AAR_LIB_TMP}
+rm -rf "$AAR_LIB_TMP"
 
 # TODO(b/122377443): Restore Makefile builds after resolving r18b build issues.
