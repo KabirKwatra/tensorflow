1ee39318dc597db483a4265a5ded163d7516a721
Restyled.io <commits@restyled.io>
2020-03-11 20:04:37 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/debug/lib/debug_events_monitors_test.py
+++ tensorflow/python/debug/lib/debug_events_monitors_test.py
@@ -17,9 +17,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl.testing import parameterized
-
 import numpy as np
+from absl.testing import parameterized
 
 from tensorflow.core.protobuf import debug_event_pb2
 from tensorflow.python.debug.lib import debug_events_monitors
--- tensorflow/python/ops/ragged/row_partition.py
+++ tensorflow/python/ops/ragged/row_partition.py
@@ -14,7 +14,6 @@
 # ==============================================================================
 """A class used to partition a sequence into contiguous subsequences ("rows").
 """
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
