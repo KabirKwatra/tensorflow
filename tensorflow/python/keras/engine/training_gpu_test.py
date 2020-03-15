560325430a3d150779a0ac2869946ad28ba859b4
Restyled.io <commits@restyled.io>
2020-03-15 06:04:44 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/keras/engine/training_gpu_test.py
+++ tensorflow/python/keras/engine/training_gpu_test.py
@@ -13,13 +13,12 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for training routines."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl.testing import parameterized
 import numpy as np
+from absl.testing import parameterized
 
 from tensorflow.python.framework import test_util
 from tensorflow.python.keras import backend as K
