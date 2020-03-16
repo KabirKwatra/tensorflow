5778023823e2b53d9cb7c1c5e165a017e15bdfe9
Restyled.io <commits@restyled.io>
2020-03-16 19:21:47 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/debug/lib/check_numerics_callback.py
+++ tensorflow/python/debug/lib/check_numerics_callback.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 """Eager-graph unified check numerics callback."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
