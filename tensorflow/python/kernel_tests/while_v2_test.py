af3dc2945e1ceb72b513abcf15dcfcf34703760b
Restyled.io <commits@restyled.io>
2020-03-18 15:22:55 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/kernel_tests/while_v2_test.py
+++ tensorflow/python/kernel_tests/while_v2_test.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for while_v2."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
