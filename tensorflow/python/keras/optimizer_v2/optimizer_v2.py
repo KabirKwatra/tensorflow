af344bfec320932f619089529828e7ad9a639a6c
Restyled.io <commits@restyled.io>
2020-03-20 19:21:11 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/keras/optimizer_v2/optimizer_v2.py
+++ tensorflow/python/keras/optimizer_v2/optimizer_v2.py
@@ -12,10 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 """Version 2 of class Optimizer."""
 # pylint: disable=g-bad-name
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
