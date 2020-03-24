99c64542daa4e7a32b3de54a188667c50d1dc002
Restyled.io <commits@restyled.io>
2020-03-24 11:22:00 +0000

    Restyled by yapf

--- tensorflow/lite/testing/op_tests/minimum.py
+++ tensorflow/lite/testing/op_tests/minimum.py
@@ -27,30 +27,28 @@ from tensorflow.lite.testing.zip_test_utils import register_make_test_function
 def make_minimum_tests(options):
     """Make a set of tests to do minimum."""
 
-    test_parameters = [
-        {
-            "input_dtype": [tf.float32],
-            "input_shape_1": [
-                [],
-                [3],
-                [1, 100],
-                [4, 2, 3],
-                [5, 224, 224, 3],
-                [5, 32, 32, 1, 1],
-                [5, 32, 32, 1, 1],
-            ],
-            "input_shape_2": [
-                [],
-                [3],
-                [1, 100],
-                [4, 2, 3],
-                [5, 224, 224, 3],
-                [5, 32, 32, 1, 1],
-                [5, 32, 32, 1, 3],
-            ],
-            "fully_quantize": [False, True],
-        }
-    ]
+    test_parameters = [{
+        "input_dtype": [tf.float32],
+        "input_shape_1": [
+            [],
+            [3],
+            [1, 100],
+            [4, 2, 3],
+            [5, 224, 224, 3],
+            [5, 32, 32, 1, 1],
+            [5, 32, 32, 1, 1],
+        ],
+        "input_shape_2": [
+            [],
+            [3],
+            [1, 100],
+            [4, 2, 3],
+            [5, 224, 224, 3],
+            [5, 32, 32, 1, 1],
+            [5, 32, 32, 1, 3],
+        ],
+        "fully_quantize": [False, True],
+    }]
 
     def build_graph(parameters):
         """Build the minimum op testing graph."""
@@ -86,6 +84,8 @@ def make_minimum_tests(options):
         ]
         return values, sess.run(outputs, feed_dict=dict(zip(inputs, values)))
 
-    make_zip_of_tests(
-        options, test_parameters, build_graph, build_inputs, expected_tf_failures=44
-    )
+    make_zip_of_tests(options,
+                      test_parameters,
+                      build_graph,
+                      build_inputs,
+                      expected_tf_failures=44)
