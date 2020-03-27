fd742af10d3d98cf35be2343cd58e1480a9edb32
Restyled.io <commits@restyled.io>
2020-03-27 03:21:11 +0000

    Restyled by clang-format

--- tensorflow/compiler/mlir/mlir_graph_optimization_pass.cc
+++ tensorflow/compiler/mlir/mlir_graph_optimization_pass.cc
@@ -36,70 +36,69 @@ limitations under the License.
 namespace tensorflow {
 
 static inline absl::string_view StringRefToView(llvm::StringRef ref) {
-    return {ref.data(), ref.size()};
+  return {ref.data(), ref.size()};
 }
 
 // Dumps the MLIR module to disk.
 // This require the TF_DUMP_GRAPH_PREFIX to be set to a path that exist (or can
 // be created).
 static void DumpModule(mlir::ModuleOp module, std::string file_prefix) {
-    std::string prefix = GetDumpDirFromEnvVar();
-    if (prefix.empty()) return;
+  std::string prefix = GetDumpDirFromEnvVar();
+  if (prefix.empty()) return;
 
-    auto* env = tensorflow::Env::Default();
-    auto status = env->RecursivelyCreateDir(prefix);
-    if (!status.ok()) {
-        LOG(WARNING) << "cannot create directory '" + prefix +
-                     "': " + status.error_message();
-        return;
-    }
+  auto* env = tensorflow::Env::Default();
+  auto status = env->RecursivelyCreateDir(prefix);
+  if (!status.ok()) {
+    LOG(WARNING) << "cannot create directory '" + prefix +
+                        "': " + status.error_message();
+    return;
+  }
 
-    prefix += "/" + file_prefix;
-    if (!tensorflow::Env::Default()->CreateUniqueFileName(&prefix, ".mlir")) {
-        LOG(WARNING) << "cannot create unique filename, won't dump MLIR module.";
-        return;
-    }
+  prefix += "/" + file_prefix;
+  if (!tensorflow::Env::Default()->CreateUniqueFileName(&prefix, ".mlir")) {
+    LOG(WARNING) << "cannot create unique filename, won't dump MLIR module.";
+    return;
+  }
 
-    std::unique_ptr<WritableFile> file_writer;
-    status = env->NewWritableFile(prefix, &file_writer);
-    if (!status.ok()) {
-        LOG(WARNING) << "cannot open file '" + prefix +
-                     "': " + status.error_message();
-        return;
-    }
+  std::unique_ptr<WritableFile> file_writer;
+  status = env->NewWritableFile(prefix, &file_writer);
+  if (!status.ok()) {
+    LOG(WARNING) << "cannot open file '" + prefix +
+                        "': " + status.error_message();
+    return;
+  }
 
-    // Print the module to a string before writing to the file.
-    std::string txt_module;
-    {
-        llvm::raw_string_ostream os(txt_module);
-        module.print(os);
-    }
+  // Print the module to a string before writing to the file.
+  std::string txt_module;
+  {
+    llvm::raw_string_ostream os(txt_module);
+    module.print(os);
+  }
 
-    status = file_writer->Append(txt_module);
-    if (!status.ok()) {
-        LOG(WARNING) << "error writing to file '" + prefix +
-                     "': " + status.error_message();
-        return;
-    }
-    (void)file_writer->Close();
-    VLOG(1) << "Dumped MLIR module to " << prefix;
+  status = file_writer->Append(txt_module);
+  if (!status.ok()) {
+    LOG(WARNING) << "error writing to file '" + prefix +
+                        "': " + status.error_message();
+    return;
+  }
+  (void)file_writer->Close();
+  VLOG(1) << "Dumped MLIR module to " << prefix;
 }
 
 MlirOptimizationPassRegistry& MlirOptimizationPassRegistry::Global() {
-    static auto* global = new MlirOptimizationPassRegistry();
-    return *global;
+  static auto* global = new MlirOptimizationPassRegistry();
+  return *global;
 }
 
 static void RegisterDialects() {
-    static bool init_once = []() {
-        mlir::registerDialect<mlir::StandardOpsDialect>();
-        mlir::registerDialect<mlir::tf_device::TensorFlowDeviceDialect>();
-        mlir::registerDialect<mlir::tf_executor::TensorFlowExecutorDialect>();
-        mlir::registerDialect<mlir::TF::TensorFlowDialect>();
-        return true;
-    }
-    ();
-    (void)init_once;
+  static bool init_once = []() {
+    mlir::registerDialect<mlir::StandardOpsDialect>();
+    mlir::registerDialect<mlir::tf_device::TensorFlowDeviceDialect>();
+    mlir::registerDialect<mlir::tf_executor::TensorFlowExecutorDialect>();
+    mlir::registerDialect<mlir::TF::TensorFlowDialect>();
+    return true;
+  }();
+  (void)init_once;
 }
 
 Status MlirFunctionOptimizationPass::Run(
@@ -107,127 +106,127 @@ Status MlirFunctionOptimizationPass::Run(
     std::unique_ptr<Graph>* graph, FunctionLibraryDefinition* flib_def,
     std::vector<std::string>* control_ret_node_names,
     bool* control_rets_updated) {
-    // Skip conversion from Graph to MLIR if none of the passes are enabled.
-    const bool is_enabled =
-    llvm::any_of(registry_->passes(), [&](auto& pass_registration) -> bool {
+  // Skip conversion from Graph to MLIR if none of the passes are enabled.
+  const bool is_enabled =
+      llvm::any_of(registry_->passes(), [&](auto& pass_registration) -> bool {
         return pass_registration.pass->IsEnabled(config_proto);
-    });
+      });
 
-    if (!is_enabled) {
-        VLOG(1) << "None of the MLIR optimization passes are enabled "
-                << "(registered " << registry_->passes().size() << ")";
-        return Status::OK();
-    }
-
-    VLOG(1) << "Running MLIR Graph Optimization Passes "
-            << "(registered " << registry_->passes().size() << " passes)";
-
-    GraphDebugInfo debug_info;
-    RegisterDialects();
-    mlir::MLIRContext context;
-    GraphImportConfig import_config;
-    import_config.graph_as_function = true;
-    import_config.control_outputs = *control_ret_node_names;
-    TF_ASSIGN_OR_RETURN(auto module_ref,
-                        ConvertGraphToMlir(**graph, debug_info, *flib_def,
-                                           import_config, &context));
-
-    AddDevicesToOp(*module_ref, &device_set);
-
-    for (auto& pass_registration : registry_->passes()) {
-        llvm::StringRef name = pass_registration.pass->name();
-        VLOG(2) << "Run MLIR graph optimization pass: " << StringRefToView(name);
-
-        if (VLOG_IS_ON(1)) {
-            DumpModule(*module_ref, llvm::formatv("mlir_{0}_before_", name));
-        }
-
-        TF_RETURN_IF_ERROR(pass_registration.pass->Run(config_proto, *module_ref));
-
-        if (VLOG_IS_ON(1)) {
-            DumpModule(*module_ref, llvm::formatv("mlir_{0}_after_", name));
-        }
-    }
-
-    GraphExportConfig export_config;
-    export_config.graph_as_function = true;
-    absl::flat_hash_set<Node*> control_ret_nodes;
-    TF_RETURN_WITH_CONTEXT_IF_ERROR(
-        ConvertMlirToGraph(*module_ref, export_config, graph, flib_def,
-                           &control_ret_nodes),
-        "Error converting MLIR module back to graph");
-
-    control_ret_node_names->clear();
-    control_ret_node_names->reserve(control_ret_nodes.size());
-    for (const auto* node : control_ret_nodes)
-        control_ret_node_names->push_back(node->name());
-
-    *control_rets_updated = true;
-
+  if (!is_enabled) {
+    VLOG(1) << "None of the MLIR optimization passes are enabled "
+            << "(registered " << registry_->passes().size() << ")";
     return Status::OK();
+  }
+
+  VLOG(1) << "Running MLIR Graph Optimization Passes "
+          << "(registered " << registry_->passes().size() << " passes)";
+
+  GraphDebugInfo debug_info;
+  RegisterDialects();
+  mlir::MLIRContext context;
+  GraphImportConfig import_config;
+  import_config.graph_as_function = true;
+  import_config.control_outputs = *control_ret_node_names;
+  TF_ASSIGN_OR_RETURN(auto module_ref,
+                      ConvertGraphToMlir(**graph, debug_info, *flib_def,
+                                         import_config, &context));
+
+  AddDevicesToOp(*module_ref, &device_set);
+
+  for (auto& pass_registration : registry_->passes()) {
+    llvm::StringRef name = pass_registration.pass->name();
+    VLOG(2) << "Run MLIR graph optimization pass: " << StringRefToView(name);
+
+    if (VLOG_IS_ON(1)) {
+      DumpModule(*module_ref, llvm::formatv("mlir_{0}_before_", name));
+    }
+
+    TF_RETURN_IF_ERROR(pass_registration.pass->Run(config_proto, *module_ref));
+
+    if (VLOG_IS_ON(1)) {
+      DumpModule(*module_ref, llvm::formatv("mlir_{0}_after_", name));
+    }
+  }
+
+  GraphExportConfig export_config;
+  export_config.graph_as_function = true;
+  absl::flat_hash_set<Node*> control_ret_nodes;
+  TF_RETURN_WITH_CONTEXT_IF_ERROR(
+      ConvertMlirToGraph(*module_ref, export_config, graph, flib_def,
+                         &control_ret_nodes),
+      "Error converting MLIR module back to graph");
+
+  control_ret_node_names->clear();
+  control_ret_node_names->reserve(control_ret_nodes.size());
+  for (const auto* node : control_ret_nodes)
+    control_ret_node_names->push_back(node->name());
+
+  *control_rets_updated = true;
+
+  return Status::OK();
 }
 
 MlirV1CompatOptimizationPassRegistry&
 MlirV1CompatOptimizationPassRegistry::Global() {
-    static auto* global = new MlirV1CompatOptimizationPassRegistry();
-    return *global;
+  static auto* global = new MlirV1CompatOptimizationPassRegistry();
+  return *global;
 }
 
 Status MlirV1CompatGraphOptimizationPass::Run(
     const GraphOptimizationPassOptions& options) {
-    // Skip function graphs as MlirOptimizationPassRegistry_ will be used instead.
-    if (options.is_function_graph) return Status::OK();
+  // Skip function graphs as MlirOptimizationPassRegistry_ will be used instead.
+  if (options.is_function_graph) return Status::OK();
 
-    // Skip conversion from Graph to MLIR if none of the passes are enabled.
-    const bool is_enabled =
-    absl::c_any_of(registry_->passes(), [&](auto& pass_registration) -> bool {
+  // Skip conversion from Graph to MLIR if none of the passes are enabled.
+  const bool is_enabled =
+      absl::c_any_of(registry_->passes(), [&](auto& pass_registration) -> bool {
         return pass_registration.pass->IsEnabled(
             options.session_options->config);
-    });
+      });
 
-    if (!is_enabled) {
-        VLOG(1) << "None of the MLIR optimization passes are enabled "
-                << "(registered" << registry_->passes().size() << " passes)";
-        return Status::OK();
-    }
-
-    VLOG(1) << "Running MLIR Graph Optimization V1 Compat Passes "
+  if (!is_enabled) {
+    VLOG(1) << "None of the MLIR optimization passes are enabled "
             << "(registered" << registry_->passes().size() << " passes)";
+    return Status::OK();
+  }
 
-    GraphDebugInfo debug_info;
-    RegisterDialects();
-    mlir::MLIRContext context;
-    GraphImportConfig import_config;
-    import_config.upgrade_legacy = true;
-    TF_ASSIGN_OR_RETURN(
-        auto module_ref,
-        ConvertGraphToMlir(**options.graph, debug_info, *options.flib_def,
-                           import_config, &context));
+  VLOG(1) << "Running MLIR Graph Optimization V1 Compat Passes "
+          << "(registered" << registry_->passes().size() << " passes)";
 
-    AddDevicesToOp(*module_ref, options.device_set);
+  GraphDebugInfo debug_info;
+  RegisterDialects();
+  mlir::MLIRContext context;
+  GraphImportConfig import_config;
+  import_config.upgrade_legacy = true;
+  TF_ASSIGN_OR_RETURN(
+      auto module_ref,
+      ConvertGraphToMlir(**options.graph, debug_info, *options.flib_def,
+                         import_config, &context));
 
-    for (auto& pass_registration : registry_->passes()) {
-        llvm::StringRef name = pass_registration.pass->name();
-        VLOG(2) << "Run MLIR graph optimization pass: " << StringRefToView(name);
+  AddDevicesToOp(*module_ref, options.device_set);
 
-        if (VLOG_IS_ON(1)) {
-            DumpModule(*module_ref, llvm::formatv("mlir_{0}_before_", name));
-        }
+  for (auto& pass_registration : registry_->passes()) {
+    llvm::StringRef name = pass_registration.pass->name();
+    VLOG(2) << "Run MLIR graph optimization pass: " << StringRefToView(name);
 
-        TF_RETURN_IF_ERROR(pass_registration.pass->Run(options, *module_ref));
-
-        if (VLOG_IS_ON(1)) {
-            DumpModule(*module_ref, llvm::formatv("mlir_{0}_after_", name));
-        }
+    if (VLOG_IS_ON(1)) {
+      DumpModule(*module_ref, llvm::formatv("mlir_{0}_before_", name));
     }
 
-    GraphExportConfig export_config;
-    TF_RETURN_WITH_CONTEXT_IF_ERROR(
-        ConvertMlirToGraph(*module_ref, export_config, options.graph,
-                           options.flib_def),
-        "Error converting MLIR module back to graph");
+    TF_RETURN_IF_ERROR(pass_registration.pass->Run(options, *module_ref));
 
-    return Status::OK();
+    if (VLOG_IS_ON(1)) {
+      DumpModule(*module_ref, llvm::formatv("mlir_{0}_after_", name));
+    }
+  }
+
+  GraphExportConfig export_config;
+  TF_RETURN_WITH_CONTEXT_IF_ERROR(
+      ConvertMlirToGraph(*module_ref, export_config, options.graph,
+                         options.flib_def),
+      "Error converting MLIR module back to graph");
+
+  return Status::OK();
 }
 
 }  // namespace tensorflow
