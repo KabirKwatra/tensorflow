4adf79865374818545c9f6598a3e707c7dd9c6b1
Restyled.io <commits@restyled.io>
2020-03-27 03:21:20 +0000

    Restyled by yapf

--- tensorflow/python/keras/layers/embeddings.py
+++ tensorflow/python/keras/layers/embeddings.py
@@ -88,28 +88,26 @@ class Embedding(Layer):
       3D tensor with shape: `(batch_size, input_length, output_dim)`.
     """
 
-    def __init__(
-        self,
-        input_dim,
-        output_dim,
-        embeddings_initializer="uniform",
-        embeddings_regularizer=None,
-        activity_regularizer=None,
-        embeddings_constraint=None,
-        mask_zero=False,
-        input_length=None,
-        **kwargs
-    ):
+    def __init__(self,
+                 input_dim,
+                 output_dim,
+                 embeddings_initializer="uniform",
+                 embeddings_regularizer=None,
+                 activity_regularizer=None,
+                 embeddings_constraint=None,
+                 mask_zero=False,
+                 input_length=None,
+                 **kwargs):
         if "input_shape" not in kwargs:
             if input_length:
-                kwargs["input_shape"] = (input_length,)
+                kwargs["input_shape"] = (input_length, )
             else:
-                kwargs["input_shape"] = (None,)
+                kwargs["input_shape"] = (None, )
         if input_dim <= 0 or output_dim <= 0:
             raise ValueError(
                 "Both `input_dim` and `output_dim` should be positive, "
-                "found input_dim {} and output_dim {}".format(input_dim, output_dim)
-            )
+                "found input_dim {} and output_dim {}".format(
+                    input_dim, output_dim))
         dtype = kwargs.pop("dtype", K.floatx())
         # We set autocast to False, as we do not want to cast floating- point inputs
         # to self.dtype. In call(), we cast to int32, and casting to self.dtype
@@ -122,8 +120,7 @@ class Embedding(Layer):
         if self.input_dim <= 0:
             raise ValueError(
                 "The argument `input_dim` should be greater than zero. "
-                "Received: %s" % input_dim
-            )
+                "Received: %s" % input_dim)
         self.output_dim = output_dim
         self.embeddings_initializer = initializers.get(embeddings_initializer)
         self.embeddings_regularizer = regularizers.get(embeddings_regularizer)
@@ -170,7 +167,7 @@ class Embedding(Layer):
     @tf_utils.shape_type_conversion
     def compute_output_shape(self, input_shape):
         if self.input_length is None:
-            return input_shape + (self.output_dim,)
+            return input_shape + (self.output_dim, )
         else:
             # input_length can be tuple if input is 3D or higher
             if isinstance(self.input_length, (list, tuple)):
@@ -178,22 +175,19 @@ class Embedding(Layer):
             else:
                 in_lens = [self.input_length]
             if len(in_lens) != len(input_shape) - 1:
-                raise ValueError(
-                    '"input_length" is %s, '
-                    "but received input has shape %s"
-                    % (str(self.input_length), str(input_shape))
-                )
+                raise ValueError('"input_length" is %s, '
+                                 "but received input has shape %s" %
+                                 (str(self.input_length), str(input_shape)))
             else:
                 for i, (s1, s2) in enumerate(zip(in_lens, input_shape[1:])):
                     if s1 is not None and s2 is not None and s1 != s2:
                         raise ValueError(
                             '"input_length" is %s, '
-                            "but received input has shape %s"
-                            % (str(self.input_length), str(input_shape))
-                        )
+                            "but received input has shape %s" %
+                            (str(self.input_length), str(input_shape)))
                     elif s1 is None:
                         in_lens[i] = s2
-            return (input_shape[0],) + tuple(in_lens) + (self.output_dim,)
+            return (input_shape[0], ) + tuple(in_lens) + (self.output_dim, )
 
     def call(self, inputs):
         dtype = K.dtype(inputs)
@@ -204,18 +198,22 @@ class Embedding(Layer):
 
     def get_config(self):
         config = {
-            "input_dim": self.input_dim,
-            "output_dim": self.output_dim,
-            "embeddings_initializer": initializers.serialize(
-                self.embeddings_initializer
-            ),
-            "embeddings_regularizer": regularizers.serialize(
-                self.embeddings_regularizer
-            ),
-            "activity_regularizer": regularizers.serialize(self.activity_regularizer),
-            "embeddings_constraint": constraints.serialize(self.embeddings_constraint),
-            "mask_zero": self.mask_zero,
-            "input_length": self.input_length,
+            "input_dim":
+            self.input_dim,
+            "output_dim":
+            self.output_dim,
+            "embeddings_initializer":
+            initializers.serialize(self.embeddings_initializer),
+            "embeddings_regularizer":
+            regularizers.serialize(self.embeddings_regularizer),
+            "activity_regularizer":
+            regularizers.serialize(self.activity_regularizer),
+            "embeddings_constraint":
+            constraints.serialize(self.embeddings_constraint),
+            "mask_zero":
+            self.mask_zero,
+            "input_length":
+            self.input_length,
         }
         base_config = super(Embedding, self).get_config()
         return dict(list(base_config.items()) + list(config.items()))
