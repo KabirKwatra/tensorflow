c2491940350ed4e7db7397cee310fb354cb63edf
Restyled.io <commits@restyled.io>
2020-03-31 01:21:58 +0000

    Restyled by autopep8

--- tensorflow/lite/python/optimize/calibrator.py
+++ tensorflow/lite/python/optimize/calibrator.py
@@ -30,121 +30,121 @@ _calibration_wrapper = LazyLoader(
 
 
 class Calibrator(object):
-  """Calibrates a floating point model and then quantizes it.
+    """Calibrates a floating point model and then quantizes it.
 
-  This is an internal class, not a public interface.
-  """
-
-  def __init__(self, model_content):
-    """Constructor.
-
-    Args:
-      model_content: Content of a TF-Lite Flatbuffer file.
-
-    Raises:
-      ValueError: If the calibrator was unable to open the model.
+    This is an internal class, not a public interface.
     """
-    if not model_content:
-      raise ValueError("`model_content` must be specified.")
-    try:
-      self._calibrator = (
-          _calibration_wrapper.CalibrationWrapper(model_content))
-    except Exception as e:
-      raise ValueError("Failed to parse the model: %s." % e)
-    if not self._calibrator:
-      raise ValueError("Failed to parse the model.")
 
-  def calibrate_and_quantize(self,
-                             dataset_gen,
-                             input_type,
-                             output_type,
-                             allow_float,
-                             resize_input=True):
-    """Calibrates the model with specified generator and then quantizes it.
+    def __init__(self, model_content):
+        """Constructor.
 
-    The input shapes of the calibrator are resized with the calibration data if
-    `resize_input` is set.
+        Args:
+          model_content: Content of a TF-Lite Flatbuffer file.
 
-    Returns:
-      A quantized model.
+        Raises:
+          ValueError: If the calibrator was unable to open the model.
+        """
+        if not model_content:
+            raise ValueError("`model_content` must be specified.")
+        try:
+            self._calibrator = (
+                _calibration_wrapper.CalibrationWrapper(model_content))
+        except Exception as e:
+            raise ValueError("Failed to parse the model: %s." % e)
+        if not self._calibrator:
+            raise ValueError("Failed to parse the model.")
 
-    Args:
-      dataset_gen: A generator that generates calibration samples.
-      input_type: A tf.dtype representing the desired real-value input type.
-      output_type: A tf.dtype representing the desired real-value output type.
-      allow_float: A boolean. False if the resulting model cannot perform float
-        computation, useful when targeting an integer-only backend. If False, an
-        error will be thrown if an operation cannot be quantized, otherwise the
-        model will fallback to float ops.
-      resize_input: A boolean. True if the shape of the sample data is different
-        from the input.
-    """
-    initialized = False
-    for sample in dataset_gen():
-      if not initialized:
-        initialized = True
-        if resize_input:
-          self._calibrator.Prepare([list(s.shape) for s in sample])
-        else:
-          self._calibrator.Prepare()
-      self._calibrator.FeedTensor(sample)
-    return self._calibrator.QuantizeModel(
-        np.dtype(input_type.as_numpy_dtype()).num,
-        np.dtype(output_type.as_numpy_dtype()).num, allow_float)
+    def calibrate_and_quantize(self,
+                               dataset_gen,
+                               input_type,
+                               output_type,
+                               allow_float,
+                               resize_input=True):
+        """Calibrates the model with specified generator and then quantizes it.
 
-  def calibrate_and_quantize_single(self,
-                                    dataset_gen,
-                                    input_type,
-                                    output_type,
-                                    allow_float,
-                                    op_output_name,
-                                    resize_input=True):
-    """Calibrates the model with specified generator and then quantizes it.
+        The input shapes of the calibrator are resized with the calibration data if
+        `resize_input` is set.
 
-    Only the single op with output op_output_name will be quantized.
-    The input shapes of the calibrator are resized with the calibration data.
+        Returns:
+          A quantized model.
 
-    Returns:
-      A quantized model.
+        Args:
+          dataset_gen: A generator that generates calibration samples.
+          input_type: A tf.dtype representing the desired real-value input type.
+          output_type: A tf.dtype representing the desired real-value output type.
+          allow_float: A boolean. False if the resulting model cannot perform float
+            computation, useful when targeting an integer-only backend. If False, an
+            error will be thrown if an operation cannot be quantized, otherwise the
+            model will fallback to float ops.
+          resize_input: A boolean. True if the shape of the sample data is different
+            from the input.
+        """
+        initialized = False
+        for sample in dataset_gen():
+            if not initialized:
+                initialized = True
+                if resize_input:
+                    self._calibrator.Prepare([list(s.shape) for s in sample])
+                else:
+                    self._calibrator.Prepare()
+            self._calibrator.FeedTensor(sample)
+        return self._calibrator.QuantizeModel(
+            np.dtype(input_type.as_numpy_dtype()).num,
+            np.dtype(output_type.as_numpy_dtype()).num, allow_float)
 
-    Args:
-      dataset_gen: A generator that generates calibration samples.
-      input_type: A tf.dtype representing the desired real-value input type.
-      output_type: A tf.dtype representing the desired real-value output type.
-      allow_float: A boolean. False if the resulting model cannot perform float
-        computation, useful when targeting an integer-only backend. If False, an
-        error will be thrown if an operation cannot be quantized, otherwise the
-        model will fallback to float ops.
-      op_output_name: A string, only this op will be quantized.
-      resize_input: A boolean. True if the shape of the sample data is different
-        from the input.
-    """
-    initialized = False
-    for sample in dataset_gen():
-      if not initialized:
-        initialized = True
-        if resize_input:
-          self._calibrator.Prepare([list(s.shape) for s in sample])
-        else:
-          self._calibrator.Prepare()
-      self._calibrator.FeedTensor(sample)
-    return self._calibrator.QuantizeModel(
-        np.dtype(input_type.as_numpy_dtype()).num,
-        np.dtype(output_type.as_numpy_dtype()).num, allow_float, op_output_name)
+    def calibrate_and_quantize_single(self,
+                                      dataset_gen,
+                                      input_type,
+                                      output_type,
+                                      allow_float,
+                                      op_output_name,
+                                      resize_input=True):
+        """Calibrates the model with specified generator and then quantizes it.
 
-  def calibrate(self, dataset_gen):
-    """Calibrates the model with specified generator.
+        Only the single op with output op_output_name will be quantized.
+        The input shapes of the calibrator are resized with the calibration data.
 
-    Returns:
-      A model with min and max calibration stats.
+        Returns:
+          A quantized model.
 
-    Args:
-      dataset_gen: A generator that generates calibration samples.
-    """
-    initialized = False
-    for sample in dataset_gen():
-      if not initialized:
-        initialized = True
-        self._calibrator.Prepare([list(s.shape) for s in sample])
-      self._calibrator.FeedTensor(sample)
-    return self._calibrator.Calibrate()
+        Args:
+          dataset_gen: A generator that generates calibration samples.
+          input_type: A tf.dtype representing the desired real-value input type.
+          output_type: A tf.dtype representing the desired real-value output type.
+          allow_float: A boolean. False if the resulting model cannot perform float
+            computation, useful when targeting an integer-only backend. If False, an
+            error will be thrown if an operation cannot be quantized, otherwise the
+            model will fallback to float ops.
+          op_output_name: A string, only this op will be quantized.
+          resize_input: A boolean. True if the shape of the sample data is different
+            from the input.
+        """
+        initialized = False
+        for sample in dataset_gen():
+            if not initialized:
+                initialized = True
+                if resize_input:
+                    self._calibrator.Prepare([list(s.shape) for s in sample])
+                else:
+                    self._calibrator.Prepare()
+            self._calibrator.FeedTensor(sample)
+        return self._calibrator.QuantizeModel(
+            np.dtype(input_type.as_numpy_dtype()).num,
+            np.dtype(output_type.as_numpy_dtype()).num, allow_float, op_output_name)
+
+    def calibrate(self, dataset_gen):
+        """Calibrates the model with specified generator.
+
+        Returns:
+          A model with min and max calibration stats.
+
+        Args:
+          dataset_gen: A generator that generates calibration samples.
+        """
+        initialized = False
+        for sample in dataset_gen():
+            if not initialized:
+                initialized = True
+                self._calibrator.Prepare([list(s.shape) for s in sample])
+            self._calibrator.FeedTensor(sample)
+        return self._calibrator.Calibrate()
