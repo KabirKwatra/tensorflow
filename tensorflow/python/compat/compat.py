e4c76a7407c0cd8b4f02d2f6ac64735b29d2090d
Restyled.io <commits@restyled.io>
2020-04-02 09:23:37 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/compat/compat.py
+++ tensorflow/python/compat/compat.py
@@ -17,7 +17,6 @@
 See [Version
 Compatibility](https://tensorflow.org/guide/version_compat#backward_forward)
 """
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
