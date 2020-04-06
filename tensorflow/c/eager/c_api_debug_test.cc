2bdda2d6160f0386ec497dd5fdd4ec66ece34894
Restyled.io <commits@restyled.io>
2020-04-06 23:51:26 +0000

    Restyled by clang-format

--- tensorflow/c/eager/c_api_debug_test.cc
+++ tensorflow/c/eager/c_api_debug_test.cc
@@ -13,50 +13,50 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#include "tensorflow/c/eager/c_api.h"
-
 #include <string.h>
+
+#include "tensorflow/c/eager/c_api.h"
 #include "tensorflow/c/eager/c_api_test_util.h"
 #include "tensorflow/core/platform/logging.h"
 #include "tensorflow/core/platform/test.h"
 
 TEST(CApiDebug, ScalarCPU) {
-    TF_Status* status = TF_NewStatus();
-    TFE_ContextOptions* opts = TFE_NewContextOptions();
-    TFE_Context* ctx = TFE_NewContext(opts, status);
-    CHECK_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
-    TFE_DeleteContextOptions(opts);
+  TF_Status* status = TF_NewStatus();
+  TFE_ContextOptions* opts = TFE_NewContextOptions();
+  TFE_Context* ctx = TFE_NewContext(opts, status);
+  CHECK_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
+  TFE_DeleteContextOptions(opts);
 
-    TFE_TensorHandle* h = TestScalarTensorHandle(ctx, 1.0f);
-    TFE_TensorDebugInfo* debug_info = TFE_TensorHandleTensorDebugInfo(h, status);
-    CHECK_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
+  TFE_TensorHandle* h = TestScalarTensorHandle(ctx, 1.0f);
+  TFE_TensorDebugInfo* debug_info = TFE_TensorHandleTensorDebugInfo(h, status);
+  CHECK_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
 
-    ASSERT_EQ(0, TFE_TensorDebugInfoOnDeviceNumDims(debug_info));
+  ASSERT_EQ(0, TFE_TensorDebugInfoOnDeviceNumDims(debug_info));
 
-    TFE_DeleteTensorDebugInfo(debug_info);
-    TFE_DeleteTensorHandle(h);
-    TFE_DeleteContext(ctx);
-    TF_DeleteStatus(status);
+  TFE_DeleteTensorDebugInfo(debug_info);
+  TFE_DeleteTensorHandle(h);
+  TFE_DeleteContext(ctx);
+  TF_DeleteStatus(status);
 }
 
 TEST(CApiDebug, 2DCPU) {
-    TF_Status* status = TF_NewStatus();
-    TFE_ContextOptions* opts = TFE_NewContextOptions();
-    TFE_Context* ctx = TFE_NewContext(opts, status);
-    CHECK_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
-    TFE_DeleteContextOptions(opts);
+  TF_Status* status = TF_NewStatus();
+  TFE_ContextOptions* opts = TFE_NewContextOptions();
+  TFE_Context* ctx = TFE_NewContext(opts, status);
+  CHECK_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
+  TFE_DeleteContextOptions(opts);
 
-    TFE_TensorHandle* h = TestMatrixTensorHandle3X2(ctx);
-    TFE_TensorDebugInfo* debug_info = TFE_TensorHandleTensorDebugInfo(h, status);
-    CHECK_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
+  TFE_TensorHandle* h = TestMatrixTensorHandle3X2(ctx);
+  TFE_TensorDebugInfo* debug_info = TFE_TensorHandleTensorDebugInfo(h, status);
+  CHECK_EQ(TF_OK, TF_GetCode(status)) << TF_Message(status);
 
-    ASSERT_EQ(2, TFE_TensorDebugInfoOnDeviceNumDims(debug_info));
-    // Shape is the same for CPU tensors.
-    EXPECT_EQ(3, TFE_TensorDebugInfoOnDeviceDim(debug_info, 0));
-    EXPECT_EQ(2, TFE_TensorDebugInfoOnDeviceDim(debug_info, 1));
+  ASSERT_EQ(2, TFE_TensorDebugInfoOnDeviceNumDims(debug_info));
+  // Shape is the same for CPU tensors.
+  EXPECT_EQ(3, TFE_TensorDebugInfoOnDeviceDim(debug_info, 0));
+  EXPECT_EQ(2, TFE_TensorDebugInfoOnDeviceDim(debug_info, 1));
 
-    TFE_DeleteTensorDebugInfo(debug_info);
-    TFE_DeleteTensorHandle(h);
-    TFE_DeleteContext(ctx);
-    TF_DeleteStatus(status);
+  TFE_DeleteTensorDebugInfo(debug_info);
+  TFE_DeleteTensorHandle(h);
+  TFE_DeleteContext(ctx);
+  TF_DeleteStatus(status);
 }
