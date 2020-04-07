7e8eefd9350271852a5cdccc17c170bb4c231e89
Restyled.io <commits@restyled.io>
2020-04-07 15:53:00 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/kernel_tests/signal/fft_ops_test.py
+++ tensorflow/python/kernel_tests/signal/fft_ops_test.py
@@ -13,15 +13,14 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for fft operations."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 import itertools
 
-from absl.testing import parameterized
 import numpy as np
+from absl.testing import parameterized
 from six.moves import xrange  # pylint: disable=redefined-builtin
 
 from tensorflow.core.protobuf import config_pb2
