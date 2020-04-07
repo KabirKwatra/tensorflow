7a23db06370f1054b4c15c24ccb8e523aeb2f1b3
Restyled.io <commits@restyled.io>
2020-04-07 03:52:08 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/eager/function_test.py
+++ tensorflow/python/eager/function_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -25,8 +24,8 @@ import sys
 import time
 import weakref
 
-from absl.testing import parameterized
 import numpy
+from absl.testing import parameterized
 
 from tensorflow.core.protobuf import config_pb2
 from tensorflow.core.protobuf import rewriter_config_pb2
