95a19ed7384812931e504502079ba012b34c1da5
Restyled.io <commits@restyled.io>
2020-04-08 21:52:31 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/pywrap_tensorflow.py
+++ tensorflow/python/pywrap_tensorflow.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 # =============================================================================
 """A Python wrapper that loads _pywrap_tensorflow_internal.so."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
