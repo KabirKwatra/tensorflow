7804f2ddae87eba81f83165a4a94b0d7e4e1d470
Restyled.io <commits@restyled.io>
2020-04-08 23:51:27 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/keras/engine/sequential_test.py
+++ tensorflow/python/keras/engine/sequential_test.py
@@ -13,13 +13,12 @@
 # limitations under the License.
 # ==============================================================================
 """Tests specific to `Sequential` model."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from absl.testing import parameterized
 import numpy as np
+from absl.testing import parameterized
 
 from tensorflow.python import keras
 from tensorflow.python.data.ops import dataset_ops
