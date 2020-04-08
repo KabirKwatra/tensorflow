40af212f720812cd1ef513b8e976c4cef53fbdec
Restyled.io <commits@restyled.io>
2020-04-08 09:51:13 +0000

    Restyled by clang-format

--- tensorflow/lite/profiling/memory_info_test.cc
+++ tensorflow/lite/profiling/memory_info_test.cc
@@ -15,6 +15,7 @@ limitations under the License.
 #include "tensorflow/lite/profiling/memory_info.h"
 
 #include <gtest/gtest.h>
+
 #include "tensorflow/lite/testing/util.h"
 
 namespace tflite {
@@ -22,49 +23,49 @@ namespace profiling {
 namespace memory {
 
 TEST(MemoryUsage, AddAndSub) {
-    MemoryUsage mem1, mem2;
-    mem1.max_rss_kb = 5;
-    mem1.total_allocated_bytes = 7000;
-    mem1.in_use_allocated_bytes = 2000;
+  MemoryUsage mem1, mem2;
+  mem1.max_rss_kb = 5;
+  mem1.total_allocated_bytes = 7000;
+  mem1.in_use_allocated_bytes = 2000;
 
-    mem2.max_rss_kb = 3;
-    mem2.total_allocated_bytes = 7000;
-    mem2.in_use_allocated_bytes = 4000;
+  mem2.max_rss_kb = 3;
+  mem2.total_allocated_bytes = 7000;
+  mem2.in_use_allocated_bytes = 4000;
 
-    const auto add_mem = mem1 + mem2;
-    EXPECT_EQ(8, add_mem.max_rss_kb);
-    EXPECT_EQ(14000, add_mem.total_allocated_bytes);
-    EXPECT_EQ(6000, add_mem.in_use_allocated_bytes);
+  const auto add_mem = mem1 + mem2;
+  EXPECT_EQ(8, add_mem.max_rss_kb);
+  EXPECT_EQ(14000, add_mem.total_allocated_bytes);
+  EXPECT_EQ(6000, add_mem.in_use_allocated_bytes);
 
-    const auto sub_mem = mem1 - mem2;
-    EXPECT_EQ(2, sub_mem.max_rss_kb);
-    EXPECT_EQ(0, sub_mem.total_allocated_bytes);
-    EXPECT_EQ(-2000, sub_mem.in_use_allocated_bytes);
+  const auto sub_mem = mem1 - mem2;
+  EXPECT_EQ(2, sub_mem.max_rss_kb);
+  EXPECT_EQ(0, sub_mem.total_allocated_bytes);
+  EXPECT_EQ(-2000, sub_mem.in_use_allocated_bytes);
 }
 
 TEST(MemoryUsage, GetMemoryUsage) {
-    MemoryUsage result;
-    EXPECT_EQ(MemoryUsage::kValueNotSet, result.max_rss_kb);
-    EXPECT_EQ(MemoryUsage::kValueNotSet, result.total_allocated_bytes);
-    EXPECT_EQ(MemoryUsage::kValueNotSet, result.in_use_allocated_bytes);
+  MemoryUsage result;
+  EXPECT_EQ(MemoryUsage::kValueNotSet, result.max_rss_kb);
+  EXPECT_EQ(MemoryUsage::kValueNotSet, result.total_allocated_bytes);
+  EXPECT_EQ(MemoryUsage::kValueNotSet, result.in_use_allocated_bytes);
 
 #ifdef __linux__
-    // Just allocate some space in heap so that we could meaningful memory usage
-    // report.
-    std::unique_ptr<int[]> int_array(new int[1204]);
-    for (int i = 0; i < 1024; ++i) int_array[i] = i;
-    result = GetMemoryUsage();
+  // Just allocate some space in heap so that we could meaningful memory usage
+  // report.
+  std::unique_ptr<int[]> int_array(new int[1204]);
+  for (int i = 0; i < 1024; ++i) int_array[i] = i;
+  result = GetMemoryUsage();
 
-    // As the getrusage call may fail, we might not be able to get max_rss_kb.
-    EXPECT_NE(MemoryUsage::kValueNotSet, result.total_allocated_bytes);
+  // As the getrusage call may fail, we might not be able to get max_rss_kb.
+  EXPECT_NE(MemoryUsage::kValueNotSet, result.total_allocated_bytes);
 #endif
 }
 
 TEST(MemoryUsage, IsSupported) {
 #ifdef __linux__
-    EXPECT_TRUE(MemoryUsage::IsSupported());
+  EXPECT_TRUE(MemoryUsage::IsSupported());
 #else
-    EXPECT_FALSE(MemoryUsage::IsSupported());
+  EXPECT_FALSE(MemoryUsage::IsSupported());
 #endif
 }
 
@@ -73,7 +74,7 @@ TEST(MemoryUsage, IsSupported) {
 }  // namespace tflite
 
 int main(int argc, char** argv) {
-    ::tflite::LogToStderr();
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::tflite::LogToStderr();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
