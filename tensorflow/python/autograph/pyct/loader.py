41dd0a252b91dd75d428490b033a337efa7ef7d3
Restyled.io <commits@restyled.io>
2020-04-14 03:50:38 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/autograph/pyct/loader.py
+++ tensorflow/python/autograph/pyct/loader.py
@@ -17,7 +17,6 @@
 
 Adapted from Tangent.
 """
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
--- tensorflow/python/autograph/pyct/loader_test.py
+++ tensorflow/python/autograph/pyct/loader_test.py
@@ -14,7 +14,6 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for loader module."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
