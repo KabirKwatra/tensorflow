f50fd17c8747cc475f196ec9cccd5217ba78f140
Restyled.io <commits@restyled.io>
2020-04-14 09:51:31 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/distribute/one_device_strategy_test.py
+++ tensorflow/python/distribute/one_device_strategy_test.py
@@ -13,10 +13,10 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for class OneDeviceStrategy."""
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 from tensorflow.python import tf2
 from tensorflow.python.data.ops import dataset_ops
 from tensorflow.python.distribute import combinations
