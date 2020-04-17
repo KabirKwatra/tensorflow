a85f4999b644793c78fa5b0a93628f3604ec4f46
Restyled.io <commits@restyled.io>
2020-04-17 03:50:21 +0000

    Restyled by astyle

--- tensorflow/lite/tools/benchmark/gpu_delegate_provider.cc
+++ tensorflow/lite/tools/benchmark/gpu_delegate_provider.cc
@@ -31,124 +31,126 @@ namespace tflite {
 namespace benchmark {
 
 class GpuDelegateProvider : public DelegateProvider {
- public:
-  GpuDelegateProvider() {
-    default_params_.AddParam("use_gpu", BenchmarkParam::Create<bool>(false));
+public:
+    GpuDelegateProvider() {
+        default_params_.AddParam("use_gpu", BenchmarkParam::Create<bool>(false));
 #if defined(__ANDROID__) || defined(REAL_IPHONE_DEVICE)
-    default_params_.AddParam("gpu_precision_loss_allowed",
-                             BenchmarkParam::Create<bool>(true));
+        default_params_.AddParam("gpu_precision_loss_allowed",
+                                 BenchmarkParam::Create<bool>(true));
 #endif
 #if defined(__ANDROID__)
-    default_params_.AddParam("gpu_experimental_enable_quant",
-                             BenchmarkParam::Create<bool>(true));
+        default_params_.AddParam("gpu_experimental_enable_quant",
+                                 BenchmarkParam::Create<bool>(true));
 #endif
 #if defined(REAL_IPHONE_DEVICE)
-    default_params_.AddParam("gpu_wait_type",
-                             BenchmarkParam::Create<std::string>(""));
+        default_params_.AddParam("gpu_wait_type",
+                                 BenchmarkParam::Create<std::string>(""));
 #endif
-  }
+    }
 
-  std::vector<Flag> CreateFlags(BenchmarkParams* params) const final;
+    std::vector<Flag> CreateFlags(BenchmarkParams* params) const final;
 
-  void LogParams(const BenchmarkParams& params) const final;
+    void LogParams(const BenchmarkParams& params) const final;
 
-  TfLiteDelegatePtr CreateTfLiteDelegate(
-      const BenchmarkParams& params) const final;
+    TfLiteDelegatePtr CreateTfLiteDelegate(
+        const BenchmarkParams& params) const final;
 
-  std::string GetName() const final { return "GPU"; }
+    std::string GetName() const final {
+        return "GPU";
+    }
 };
 REGISTER_DELEGATE_PROVIDER(GpuDelegateProvider);
 
 std::vector<Flag> GpuDelegateProvider::CreateFlags(
     BenchmarkParams* params) const {
-  std::vector<Flag> flags = {
-    CreateFlag<bool>("use_gpu", params, "use gpu"),
+    std::vector<Flag> flags = {
+        CreateFlag<bool>("use_gpu", params, "use gpu"),
 #if defined(__ANDROID__) || defined(REAL_IPHONE_DEVICE)
-    CreateFlag<bool>("gpu_precision_loss_allowed", params,
-                     "Allow to process computation in lower precision than "
-                     "FP32 in GPU. By default, it's enabled."),
+        CreateFlag<bool>("gpu_precision_loss_allowed", params,
+                         "Allow to process computation in lower precision than "
+                         "FP32 in GPU. By default, it's enabled."),
 #endif
 #if defined(__ANDROID__)
-    CreateFlag<bool>("gpu_experimental_enable_quant", params,
-                     "Whether to enable the GPU delegate to run quantized "
-                     "models or not. By default, it's disabled."),
+        CreateFlag<bool>("gpu_experimental_enable_quant", params,
+                         "Whether to enable the GPU delegate to run quantized "
+                         "models or not. By default, it's disabled."),
 #endif
 #if defined(REAL_IPHONE_DEVICE)
-    CreateFlag<std::string>(
-        "gpu_wait_type", params,
-        "GPU wait type. Should be one of the following: passive, active, "
-        "do_not_wait, aggressive"),
+        CreateFlag<std::string>(
+            "gpu_wait_type", params,
+            "GPU wait type. Should be one of the following: passive, active, "
+            "do_not_wait, aggressive"),
 #endif
-  };
-  return flags;
+    };
+    return flags;
 }
 
 void GpuDelegateProvider::LogParams(const BenchmarkParams& params) const {
-  TFLITE_LOG(INFO) << "Use gpu : [" << params.Get<bool>("use_gpu") << "]";
+    TFLITE_LOG(INFO) << "Use gpu : [" << params.Get<bool>("use_gpu") << "]";
 #if defined(__ANDROID__) || defined(REAL_IPHONE_DEVICE)
-  TFLITE_LOG(INFO) << "Allow lower precision in gpu : ["
-                   << params.Get<bool>("gpu_precision_loss_allowed") << "]";
+    TFLITE_LOG(INFO) << "Allow lower precision in gpu : ["
+                     << params.Get<bool>("gpu_precision_loss_allowed") << "]";
 #endif
 #if defined(__ANDROID__)
-  TFLITE_LOG(INFO) << "Enable running quant models in gpu : ["
-                   << params.Get<bool>("gpu_experimental_enable_quant") << "]";
+    TFLITE_LOG(INFO) << "Enable running quant models in gpu : ["
+                     << params.Get<bool>("gpu_experimental_enable_quant") << "]";
 #endif
 #if defined(REAL_IPHONE_DEVICE)
-  TFLITE_LOG(INFO) << "GPU delegate wait type : ["
-                   << params.Get<std::string>("gpu_wait_type") << "]";
+    TFLITE_LOG(INFO) << "GPU delegate wait type : ["
+                     << params.Get<std::string>("gpu_wait_type") << "]";
 #endif
 }
 
 TfLiteDelegatePtr GpuDelegateProvider::CreateTfLiteDelegate(
     const BenchmarkParams& params) const {
-  TfLiteDelegatePtr delegate(nullptr, [](TfLiteDelegate*) {});
+    TfLiteDelegatePtr delegate(nullptr, [](TfLiteDelegate*) {});
 
-  if (params.Get<bool>("use_gpu")) {
+    if (params.Get<bool>("use_gpu")) {
 #if defined(__ANDROID__)
-    TfLiteGpuDelegateOptionsV2 gpu_opts = TfLiteGpuDelegateOptionsV2Default();
-    if (params.Get<bool>("gpu_precision_loss_allowed")) {
-      gpu_opts.inference_priority1 = TFLITE_GPU_INFERENCE_PRIORITY_MIN_LATENCY;
-      gpu_opts.inference_priority2 =
-          TFLITE_GPU_INFERENCE_PRIORITY_MIN_MEMORY_USAGE;
-      gpu_opts.inference_priority3 =
-          TFLITE_GPU_INFERENCE_PRIORITY_MAX_PRECISION;
-    }
-    if (params.Get<bool>("gpu_experimental_enable_quant")) {
-      gpu_opts.experimental_flags |= TFLITE_GPU_EXPERIMENTAL_FLAGS_ENABLE_QUANT;
-    }
-    delegate = evaluation::CreateGPUDelegate(&gpu_opts);
+        TfLiteGpuDelegateOptionsV2 gpu_opts = TfLiteGpuDelegateOptionsV2Default();
+        if (params.Get<bool>("gpu_precision_loss_allowed")) {
+            gpu_opts.inference_priority1 = TFLITE_GPU_INFERENCE_PRIORITY_MIN_LATENCY;
+            gpu_opts.inference_priority2 =
+                TFLITE_GPU_INFERENCE_PRIORITY_MIN_MEMORY_USAGE;
+            gpu_opts.inference_priority3 =
+                TFLITE_GPU_INFERENCE_PRIORITY_MAX_PRECISION;
+        }
+        if (params.Get<bool>("gpu_experimental_enable_quant")) {
+            gpu_opts.experimental_flags |= TFLITE_GPU_EXPERIMENTAL_FLAGS_ENABLE_QUANT;
+        }
+        delegate = evaluation::CreateGPUDelegate(&gpu_opts);
 #elif defined(REAL_IPHONE_DEVICE)
-    TFLGpuDelegateOptions gpu_opts = {0};
-    gpu_opts.allow_precision_loss =
-        params.Get<bool>("gpu_precision_loss_allowed");
+        TFLGpuDelegateOptions gpu_opts = {0};
+        gpu_opts.allow_precision_loss =
+            params.Get<bool>("gpu_precision_loss_allowed");
 
-    std::string string_gpu_wait_type = params.Get<std::string>("gpu_wait_type");
-    if (!string_gpu_wait_type.empty()) {
-      TFLGpuDelegateWaitType wait_type = TFLGpuDelegateWaitTypePassive;
-      if (string_gpu_wait_type == "passive") {
-        wait_type = TFLGpuDelegateWaitTypePassive;
-      } else if (string_gpu_wait_type == "active") {
-        wait_type = TFLGpuDelegateWaitTypeActive;
-      } else if (string_gpu_wait_type == "do_not_wait") {
-        wait_type = TFLGpuDelegateWaitTypeDoNotWait;
-      } else if (string_gpu_wait_type == "aggressive") {
-        wait_type = TFLGpuDelegateWaitTypeAggressive;
-      }
-      gpu_opts.wait_type = wait_type;
-    }
-    delegate = TfLiteDelegatePtr(TFLGpuDelegateCreate(&gpu_opts),
-                                 &TFLGpuDelegateDelete);
+        std::string string_gpu_wait_type = params.Get<std::string>("gpu_wait_type");
+        if (!string_gpu_wait_type.empty()) {
+            TFLGpuDelegateWaitType wait_type = TFLGpuDelegateWaitTypePassive;
+            if (string_gpu_wait_type == "passive") {
+                wait_type = TFLGpuDelegateWaitTypePassive;
+            } else if (string_gpu_wait_type == "active") {
+                wait_type = TFLGpuDelegateWaitTypeActive;
+            } else if (string_gpu_wait_type == "do_not_wait") {
+                wait_type = TFLGpuDelegateWaitTypeDoNotWait;
+            } else if (string_gpu_wait_type == "aggressive") {
+                wait_type = TFLGpuDelegateWaitTypeAggressive;
+            }
+            gpu_opts.wait_type = wait_type;
+        }
+        delegate = TfLiteDelegatePtr(TFLGpuDelegateCreate(&gpu_opts),
+                                     &TFLGpuDelegateDelete);
 #else
-    TFLITE_LOG(WARN) << "The GPU delegate compile options are only supported "
-                        "to be benchmarked on Android or iOS platforms.";
-    delegate = evaluation::CreateGPUDelegate();
+        TFLITE_LOG(WARN) << "The GPU delegate compile options are only supported "
+                         "to be benchmarked on Android or iOS platforms.";
+        delegate = evaluation::CreateGPUDelegate();
 #endif
 
-    if (!delegate.get()) {
-      TFLITE_LOG(WARN) << "GPU acceleration is unsupported on this platform.";
+        if (!delegate.get()) {
+            TFLITE_LOG(WARN) << "GPU acceleration is unsupported on this platform.";
+        }
     }
-  }
-  return delegate;
+    return delegate;
 }
 
 }  // namespace benchmark
