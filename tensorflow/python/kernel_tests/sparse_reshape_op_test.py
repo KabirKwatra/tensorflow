4efb1b139d6af69811fc1bfdfef08a5d22d11a89
Restyled.io <commits@restyled.io>
2020-04-21 05:51:14 +0000

    Restyled by yapf

--- tensorflow/python/kernel_tests/sparse_reshape_op_test.py
+++ tensorflow/python/kernel_tests/sparse_reshape_op_test.py
@@ -39,39 +39,36 @@ class SparseReshapeTest(test.TestCase):
         )
 
     def _SparseTensorValue_5x6(self):
-        ind = np.array([[0, 0], [1, 0], [1, 3], [1, 4], [3, 2], [3, 3]]).astype(
-            np.int64
-        )
+        ind = np.array([[0, 0], [1, 0], [1, 3], [1, 4], [3, 2],
+                        [3, 3]]).astype(np.int64)
         val = np.array([0, 10, 13, 14, 32, 33]).astype(np.float64)
         shape = np.array([5, 6]).astype(np.int64)
         return sparse_tensor.SparseTensorValue(ind, val, shape)
 
     def _SparseTensorValue_2x3x4(self):
-        ind = np.array(
-            [
-                [0, 0, 1],
-                [0, 1, 0],
-                [0, 1, 2],
-                [1, 0, 3],
-                [1, 1, 1],
-                [1, 1, 3],
-                [1, 2, 2],
-            ]
-        )
+        ind = np.array([
+            [0, 0, 1],
+            [0, 1, 0],
+            [0, 1, 2],
+            [1, 0, 3],
+            [1, 1, 1],
+            [1, 1, 3],
+            [1, 2, 2],
+        ])
         val = np.array([1, 10, 12, 103, 111, 113, 122])
         shape = np.array([2, 3, 4])
         return sparse_tensor.SparseTensorValue(ind, val, shape)
 
     def testStaticShapeInfoPreserved(self):
-        sp_input = sparse_tensor.SparseTensor.from_value(self._SparseTensorValue_5x6())
+        sp_input = sparse_tensor.SparseTensor.from_value(
+            self._SparseTensorValue_5x6())
         self.assertAllEqual((5, 6), sp_input.get_shape())
         sp_output = sparse_ops.sparse_reshape(sp_input, shape=(1, 5, 2, 3))
         self.assertAllEqual((1, 5, 2, 3), sp_output.get_shape())
 
     def testStaticShapeInfoPreservedWithInferredDims(self):
         sp_input = sparse_tensor.SparseTensor.from_value(
-            self._SparseTensorValue_2x3x4()
-        )
+            self._SparseTensorValue_2x3x4())
         self.assertAllEqual((2, 3, 4), sp_input.get_shape())
         sp_output = sparse_ops.sparse_reshape(sp_input, shape=(2, -1))
         self.assertAllEqual((2, 3 * 4), sp_output.get_shape())
@@ -79,24 +76,21 @@ class SparseReshapeTest(test.TestCase):
     @test_util.run_deprecated_v1
     def testRaisesIfMoreThanOneInferredDim(self):
         sp_input = sparse_tensor.SparseTensor.from_value(
-            self._SparseTensorValue_2x3x4()
-        )
+            self._SparseTensorValue_2x3x4())
         with self.assertRaisesRegexp(ValueError, "At most one dimension can"):
             sparse_ops.sparse_reshape(sp_input, shape=(-1, 2, -1))
 
     @test_util.run_deprecated_v1
     def testRaisesIfInferredShapeNotPossible(self):
         sp_input = sparse_tensor.SparseTensor.from_value(
-            self._SparseTensorValue_2x3x4()
-        )
+            self._SparseTensorValue_2x3x4())
         with self.assertRaisesRegexp(ValueError, "Cannot reshape"):
             sparse_ops.sparse_reshape(sp_input, shape=(-1, 7))
 
     @test_util.run_deprecated_v1
     def testPropagatesFullyKnownDenseShapeWhenShapePartiallyKnown(self):
         sp_input = sparse_tensor.SparseTensor.from_value(
-            self._SparseTensorValue_2x3x4()
-        )
+            self._SparseTensorValue_2x3x4())
         self.assertAllEqual((2, 3, 4), sp_input.shape)
         sp_output = sparse_ops.sparse_reshape(
             sp_input,
@@ -196,9 +190,8 @@ class SparseReshapeTest(test.TestCase):
             output_val = self.evaluate(sp_output)
             self.assertAllEqual(
                 output_val.indices,
-                np.array(
-                    [[0, 0, 0], [0, 1, 1], [0, 1, 4], [0, 2, 0], [1, 1, 0], [1, 1, 1]]
-                ),
+                np.array([[0, 0, 0], [0, 1, 1], [0, 1, 4], [0, 2, 0],
+                          [1, 1, 0], [1, 1, 1]]),
             )
             self.assertAllEqual(output_val.values, input_val.values)
             self.assertAllEqual(output_val.dense_shape, [2, 3, 5])
@@ -213,9 +206,8 @@ class SparseReshapeTest(test.TestCase):
             output_val = sess.run(sp_output, {sp_input: input_val})
             self.assertAllEqual(
                 output_val.indices,
-                np.array(
-                    [[0, 0, 0], [0, 1, 1], [0, 1, 4], [0, 2, 0], [1, 1, 0], [1, 1, 1]]
-                ),
+                np.array([[0, 0, 0], [0, 1, 1], [0, 1, 4], [0, 2, 0],
+                          [1, 1, 0], [1, 1, 1]]),
             )
             self.assertAllEqual(output_val.values, input_val.values)
             self.assertAllEqual(output_val.dense_shape, [2, 3, 5])
@@ -230,9 +222,8 @@ class SparseReshapeTest(test.TestCase):
             output_val = sess.run(sp_output, {sp_input: input_val})
             self.assertAllEqual(
                 output_val.indices,
-                np.array(
-                    [[0, 0, 0], [0, 1, 1], [0, 1, 4], [0, 2, 0], [1, 1, 0], [1, 1, 1]]
-                ),
+                np.array([[0, 0, 0], [0, 1, 1], [0, 1, 4], [0, 2, 0],
+                          [1, 1, 0], [1, 1, 1]]),
             )
             self.assertAllEqual(output_val.values, input_val.values)
             self.assertAllEqual(output_val.dense_shape, [2, 3, 5])
@@ -247,7 +238,8 @@ class SparseReshapeTest(test.TestCase):
             output_val = sess.run(sp_output, {sp_input: input_val})
             self.assertAllEqual(
                 output_val.indices,
-                np.array([[0, 1], [1, 0], [1, 2], [3, 3], [4, 1], [4, 3], [5, 2]]),
+                np.array([[0, 1], [1, 0], [1, 2], [3, 3], [4, 1], [4, 3],
+                          [5, 2]]),
             )
             self.assertAllEqual(output_val.values, input_val.values)
             self.assertAllEqual(output_val.dense_shape, [6, 4])
@@ -262,7 +254,8 @@ class SparseReshapeTest(test.TestCase):
             output_val = sess.run(sp_output, {sp_input: input_val})
             self.assertAllEqual(
                 output_val.indices,
-                np.array([[0, 1], [1, 0], [1, 2], [3, 3], [4, 1], [4, 3], [5, 2]]),
+                np.array([[0, 1], [1, 0], [1, 2], [3, 3], [4, 1], [4, 3],
+                          [5, 2]]),
             )
             self.assertAllEqual(output_val.values, input_val.values)
             self.assertAllEqual(output_val.dense_shape, [6, 4])
@@ -273,7 +266,8 @@ class SparseReshapeTest(test.TestCase):
             sp_input = self._SparseTensorPlaceholder()
             input_val = self._SparseTensorValue_5x6()
             sp_output = sparse_ops.sparse_reshape(sp_input, [4, -1, -1])
-            with self.assertRaisesOpError("only one output dimension may be -1"):
+            with self.assertRaisesOpError(
+                    "only one output dimension may be -1"):
                 sess.run(sp_output, {sp_input: input_val})
 
     @test_util.run_deprecated_v1
@@ -290,8 +284,7 @@ class SparseReshapeTest(test.TestCase):
             input_val = self._SparseTensorValue_5x6()
             sp_output = sparse_ops.sparse_reshape(sp_input, [4, 7])
             with self.assertRaisesOpError(
-                "Input to reshape is a tensor with 30 dense values"
-            ):
+                    "Input to reshape is a tensor with 30 dense values"):
                 sess.run(sp_output, {sp_input: input_val})
 
     @test_util.run_deprecated_v1
@@ -300,7 +293,8 @@ class SparseReshapeTest(test.TestCase):
             sp_input = self._SparseTensorPlaceholder()
             input_val = self._SparseTensorValue_5x6()
             sp_output = sparse_ops.sparse_reshape(sp_input, [4, -1])
-            with self.assertRaisesOpError("requested shape requires a multiple"):
+            with self.assertRaisesOpError(
+                    "requested shape requires a multiple"):
                 sess.run(sp_output, {sp_input: input_val})
 
     @test_util.run_deprecated_v1
@@ -309,16 +303,20 @@ class SparseReshapeTest(test.TestCase):
             # Incorporate new rank into shape information if known
             sp_input = self._SparseTensorPlaceholder()
             sp_output = sparse_ops.sparse_reshape(sp_input, [2, 3, 5])
-            self.assertListEqual(sp_output.indices.get_shape().as_list(), [None, 3])
-            self.assertListEqual(sp_output.dense_shape.get_shape().as_list(), [3])
+            self.assertListEqual(sp_output.indices.get_shape().as_list(),
+                                 [None, 3])
+            self.assertListEqual(sp_output.dense_shape.get_shape().as_list(),
+                                 [3])
 
             # Incorporate known shape information about input indices in output
             # indices
             sp_input = self._SparseTensorPlaceholder()
             sp_input.indices.set_shape([5, None])
             sp_output = sparse_ops.sparse_reshape(sp_input, [2, 3, 5])
-            self.assertListEqual(sp_output.indices.get_shape().as_list(), [5, 3])
-            self.assertListEqual(sp_output.dense_shape.get_shape().as_list(), [3])
+            self.assertListEqual(sp_output.indices.get_shape().as_list(),
+                                 [5, 3])
+            self.assertListEqual(sp_output.dense_shape.get_shape().as_list(),
+                                 [3])
 
             # Even if new_shape has no shape information, we know the ranks of
             # output indices and shape
@@ -326,8 +324,10 @@ class SparseReshapeTest(test.TestCase):
             sp_input.indices.set_shape([5, None])
             new_shape = array_ops.placeholder(dtypes.int64)
             sp_output = sparse_ops.sparse_reshape(sp_input, new_shape)
-            self.assertListEqual(sp_output.indices.get_shape().as_list(), [5, None])
-            self.assertListEqual(sp_output.dense_shape.get_shape().as_list(), [None])
+            self.assertListEqual(sp_output.indices.get_shape().as_list(),
+                                 [5, None])
+            self.assertListEqual(sp_output.dense_shape.get_shape().as_list(),
+                                 [None])
 
     @test_util.run_deprecated_v1
     def testFeedDenseReshapeSemantics(self):
@@ -335,13 +335,18 @@ class SparseReshapeTest(test.TestCase):
             # Compute a random rank-5 initial shape and new shape, randomly sparsify
             # it, and check that the output of SparseReshape has the same semantics
             # as a dense reshape.
-            factors = np.array([2] * 4 + [3] * 4 + [5] * 4)  # 810k total elements
+            factors = np.array([2] * 4 + [3] * 4 +
+                               [5] * 4)  # 810k total elements
             orig_rank = np.random.randint(2, 7)
             orig_map = np.random.randint(orig_rank, size=factors.shape)
-            orig_shape = [np.prod(factors[orig_map == d]) for d in range(orig_rank)]
+            orig_shape = [
+                np.prod(factors[orig_map == d]) for d in range(orig_rank)
+            ]
             new_rank = np.random.randint(2, 7)
             new_map = np.random.randint(new_rank, size=factors.shape)
-            new_shape = [np.prod(factors[new_map == d]) for d in range(new_rank)]
+            new_shape = [
+                np.prod(factors[new_map == d]) for d in range(new_rank)
+            ]
 
             orig_dense = np.random.uniform(size=orig_shape)
             orig_indices = np.transpose(np.nonzero(orig_dense < 0.5))
@@ -353,8 +358,7 @@ class SparseReshapeTest(test.TestCase):
 
             sp_input = self._SparseTensorPlaceholder()
             input_val = sparse_tensor.SparseTensorValue(
-                orig_indices, orig_values, orig_shape
-            )
+                orig_indices, orig_values, orig_shape)
             sp_output = sparse_ops.sparse_reshape(sp_input, new_shape)
 
             output_val = sess.run(sp_output, {sp_input: input_val})
@@ -366,55 +370,61 @@ class SparseReshapeTest(test.TestCase):
 class EmptySparseTensorReshapeTest(test.TestCase, parameterized.TestCase):
     """Tests for reshaping 0-sized SparseTensors, compared w/ dense tensors."""
 
-    def _MakeAndReshapeTensor(self, tensor_class, original_shape, target_shape):
+    def _MakeAndReshapeTensor(self, tensor_class, original_shape,
+                              target_shape):
         if tensor_class == "sparse":
             ind = np.zeros([0, len(original_shape)]).astype(np.int64)
             val = np.array([]).astype(np.float64)
             shape = np.array(original_shape).astype(np.int64)
             sp_input = sparse_tensor.SparseTensorValue(ind, val, shape)
-            sp_output = self.evaluate(sparse_ops.sparse_reshape(sp_input, target_shape))
+            sp_output = self.evaluate(
+                sparse_ops.sparse_reshape(sp_input, target_shape))
             return sp_output.dense_shape
         else:
             dense_input = array_ops.zeros(original_shape)
-            dense_output = self.evaluate(array_ops.reshape(dense_input, target_shape))
+            dense_output = self.evaluate(
+                array_ops.reshape(dense_input, target_shape))
             return dense_output.shape
 
-    @parameterized.named_parameters(
-        [("Dense", "dense"), ("Sparse", "sparse"),]
-    )
+    @parameterized.named_parameters([
+        ("Dense", "dense"),
+        ("Sparse", "sparse"),
+    ])
     def testImpliedReshapeEmpty1DTensor(self, tensor_class):
         self.assertAllEqual(
-            self._MakeAndReshapeTensor(tensor_class, [0], [-1, 1]), [0, 1]
-        )
+            self._MakeAndReshapeTensor(tensor_class, [0], [-1, 1]), [0, 1])
         self.assertAllEqual(
-            self._MakeAndReshapeTensor(tensor_class, [0], [-1, 1, 2]), [0, 1, 2]
-        )
+            self._MakeAndReshapeTensor(tensor_class, [0], [-1, 1, 2]),
+            [0, 1, 2])
 
-    @parameterized.named_parameters(
-        [("Dense", "dense"), ("Sparse", "sparse"),]
-    )
+    @parameterized.named_parameters([
+        ("Dense", "dense"),
+        ("Sparse", "sparse"),
+    ])
     def testImpliedReshapeEmpty2DTensor(self, tensor_class):
         self.assertAllEqual(
-            self._MakeAndReshapeTensor(tensor_class, [1, 0], [-1, 1]), [0, 1]
-        )
+            self._MakeAndReshapeTensor(tensor_class, [1, 0], [-1, 1]), [0, 1])
         self.assertAllEqual(
-            self._MakeAndReshapeTensor(tensor_class, [1, 0], [-1, 2, 3]), [0, 2, 3]
-        )
+            self._MakeAndReshapeTensor(tensor_class, [1, 0], [-1, 2, 3]),
+            [0, 2, 3])
 
-    @parameterized.named_parameters(
-        [("Dense", "dense"), ("Sparse", "sparse"),]
-    )
+    @parameterized.named_parameters([
+        ("Dense", "dense"),
+        ("Sparse", "sparse"),
+    ])
     def testImpliedReshapeEmpty3DTensor(self, tensor_class):
         self.assertAllEqual(
-            self._MakeAndReshapeTensor(tensor_class, [1, 0, 0], [-1, 2, 3]), [0, 2, 3]
-        )
+            self._MakeAndReshapeTensor(tensor_class, [1, 0, 0], [-1, 2, 3]),
+            [0, 2, 3])
 
-    @parameterized.named_parameters(
-        [("Dense", "dense"), ("Sparse", "sparse"),]
-    )
+    @parameterized.named_parameters([
+        ("Dense", "dense"),
+        ("Sparse", "sparse"),
+    ])
     def testImpliedReshapeEmpty4DTensor(self, tensor_class):
         self.assertAllEqual(
-            self._MakeAndReshapeTensor(tensor_class, [2, 4, 0, 6], [-1, 4, 6, 2]),
+            self._MakeAndReshapeTensor(tensor_class, [2, 4, 0, 6],
+                                       [-1, 4, 6, 2]),
             [0, 4, 6, 2],
         )
 
