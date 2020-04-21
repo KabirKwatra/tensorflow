20c53a1ce8e106013deacca4666d1765ce1d7e49
Restyled.io <commits@restyled.io>
2020-04-21 05:51:05 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/kernel_tests/sparse_reshape_op_test.py
+++ tensorflow/python/kernel_tests/sparse_reshape_op_test.py
@@ -13,14 +13,20 @@
 # limitations under the License.
 # ==============================================================================
 """Tests for SparseReshape."""
-
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 import numpy as np
 from absl.testing import parameterized
-from tensorflow.python.framework import (constant_op, dtypes, errors,
-                                         sparse_tensor, test_util)
-from tensorflow.python.ops import array_ops, sparse_ops
+
+from tensorflow.python.framework import constant_op
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import errors
+from tensorflow.python.framework import sparse_tensor
+from tensorflow.python.framework import test_util
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import sparse_ops
 from tensorflow.python.platform import test
 
 
