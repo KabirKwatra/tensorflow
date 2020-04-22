477e9fe488348248ef445fa6c10adb9adca235fb
Restyled.io <commits@restyled.io>
2020-04-22 18:20:20 +0000

    Restyled by astyle

--- tensorflow/python/distribute/parallel_device/pywrap_parallel_device.cc
+++ tensorflow/python/distribute/parallel_device/pywrap_parallel_device.cc
@@ -29,42 +29,42 @@ limitations under the License.
 namespace py = pybind11;
 
 void CallDelete_Device(PyObject* capsule) {
-  delete reinterpret_cast<TFE_CustomDevice*>(
-      PyCapsule_GetPointer(capsule, "TFE_CustomDevice"));
+    delete reinterpret_cast<TFE_CustomDevice*>(
+        PyCapsule_GetPointer(capsule, "TFE_CustomDevice"));
 }
 
 void CallDelete_DeviceInfo(PyObject* capsule) {
-  void (*destructor)(void*) =
-      reinterpret_cast<void (*)(void*)>(PyCapsule_GetContext(capsule));
-  destructor(PyCapsule_GetPointer(capsule, "TFE_CustomDevice_DeviceInfo"));
+    void (*destructor)(void*) =
+        reinterpret_cast<void (*)(void*)>(PyCapsule_GetContext(capsule));
+    destructor(PyCapsule_GetPointer(capsule, "TFE_CustomDevice_DeviceInfo"));
 }
 
 PYBIND11_MODULE(_pywrap_parallel_device, m) {
-  m.def("GetParallelDeviceCapsules",
-        [](const char* name, std::vector<std::string> underlying_devices) {
-          std::vector<const char*> underlying_devices_c;
-          underlying_devices_c.reserve(underlying_devices.size());
-          for (const std::string& element : underlying_devices) {
+    m.def("GetParallelDeviceCapsules",
+    [](const char* name, std::vector<std::string> underlying_devices) {
+        std::vector<const char*> underlying_devices_c;
+        underlying_devices_c.reserve(underlying_devices.size());
+        for (const std::string& element : underlying_devices) {
             underlying_devices_c.push_back(element.c_str());
-          }
-          // `device` is owned by `device_capsule`.
-          TFE_CustomDevice* device = new TFE_CustomDevice;
-          tensorflow::Safe_PyObjectPtr device_capsule(
-              PyCapsule_New(device, "TFE_CustomDevice", &CallDelete_Device));
-          void* device_info;
-          tensorflow::eager::AllocateParallelDevice(
-              name, underlying_devices_c.data(), underlying_devices_c.size(),
-              device, &device_info);
-          if (PyErr_Occurred()) throw py::error_already_set();
-          tensorflow::Safe_PyObjectPtr device_info_capsule(
-              PyCapsule_New(device_info, "TFE_CustomDevice_DeviceInfo",
-                            &CallDelete_DeviceInfo));
-          if (PyErr_Occurred()) throw py::error_already_set();
-          // The PyCapsule destructor needs a pointer to the destructor for
-          // DeviceInfo.
-          PyCapsule_SetContext(device_info_capsule.get(),
-                               reinterpret_cast<void*>(device->delete_device));
-          return tensorflow::PyoOrThrow(
-              PyTuple_Pack(2, device_capsule.get(), device_info_capsule.get()));
-        });
+        }
+        // `device` is owned by `device_capsule`.
+        TFE_CustomDevice* device = new TFE_CustomDevice;
+        tensorflow::Safe_PyObjectPtr device_capsule(
+            PyCapsule_New(device, "TFE_CustomDevice", &CallDelete_Device));
+        void* device_info;
+        tensorflow::eager::AllocateParallelDevice(
+            name, underlying_devices_c.data(), underlying_devices_c.size(),
+            device, &device_info);
+        if (PyErr_Occurred()) throw py::error_already_set();
+        tensorflow::Safe_PyObjectPtr device_info_capsule(
+            PyCapsule_New(device_info, "TFE_CustomDevice_DeviceInfo",
+                          &CallDelete_DeviceInfo));
+        if (PyErr_Occurred()) throw py::error_already_set();
+        // The PyCapsule destructor needs a pointer to the destructor for
+        // DeviceInfo.
+        PyCapsule_SetContext(device_info_capsule.get(),
+                             reinterpret_cast<void*>(device->delete_device));
+        return tensorflow::PyoOrThrow(
+                   PyTuple_Pack(2, device_capsule.get(), device_info_capsule.get()));
+    });
 }
