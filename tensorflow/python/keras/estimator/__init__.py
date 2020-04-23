70e8d376732168ebdb67abbf8200d0a4122451fa
Restyled.io <commits@restyled.io>
2020-04-23 06:20:06 +0000

    Restyled by reorder-python-imports

--- tensorflow/python/keras/estimator/__init__.py
+++ tensorflow/python/keras/estimator/__init__.py
@@ -13,8 +13,9 @@
 # limitations under the License.
 # ==============================================================================
 """Keras estimator API."""
-
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
 
 from tensorflow.python.eager import monitoring
 from tensorflow.python.util.tf_export import keras_export
