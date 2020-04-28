cf3f383e70dd5b0136fc2fbc10e39bcba2d1c2a8
Restyled.io <commits@restyled.io>
2020-04-28 02:20:17 +0000

    Restyled by isort

--- tensorflow/python/framework/test_util.py
+++ tensorflow/python/framework/test_util.py
@@ -15,12 +15,9 @@
 
 # pylint: disable=invalid-name
 """Test utils for tensorflow."""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
 import collections
-from collections import OrderedDict
 import contextlib
 import functools
 import gc
@@ -33,55 +30,35 @@ import tempfile
 import threading
 import time
 import unittest
+from collections import OrderedDict
 
-from absl.testing import parameterized
 import numpy as np
 import six
-
-from google.protobuf import descriptor_pool
-from google.protobuf import text_format
-
+from absl.testing import parameterized
+from google.protobuf import descriptor_pool, text_format
 from tensorflow.core.framework import graph_pb2
 from tensorflow.core.protobuf import rewriter_config_pb2
-from tensorflow.python import _pywrap_stacktrace_handler
-from tensorflow.python import _pywrap_util_port
-from tensorflow.python import tf2
-from tensorflow.python.client import device_lib
-from tensorflow.python.client import pywrap_tf_session
-from tensorflow.python.client import session
+from tensorflow.python import (_pywrap_stacktrace_handler, _pywrap_util_port,
+                               tf2)
+from tensorflow.python.client import device_lib, pywrap_tf_session, session
 from tensorflow.python.compat.compat import forward_compatibility_horizon
-from tensorflow.python.eager import context
-from tensorflow.python.eager import def_function
-from tensorflow.python.eager import tape
+from tensorflow.python.eager import context, def_function, tape
 from tensorflow.python.framework import device as pydev
-from tensorflow.python.framework import dtypes
-from tensorflow.python.framework import errors
-from tensorflow.python.framework import errors_impl
-from tensorflow.python.framework import gpu_util
-from tensorflow.python.framework import importer
-from tensorflow.python.framework import ops
-from tensorflow.python.framework import random_seed
-from tensorflow.python.framework import sparse_tensor
-from tensorflow.python.framework import tensor_shape
-from tensorflow.python.framework import tensor_util
-from tensorflow.python.framework import versions
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import control_flow_util
-from tensorflow.python.ops import control_flow_util_v2
-from tensorflow.python.ops import script_ops
-from tensorflow.python.ops import summary_ops_v2
-from tensorflow.python.ops import variables
-from tensorflow.python.ops.ragged import ragged_ops  # pylint: disable=unused-import
-from tensorflow.python.ops.ragged import ragged_tensor
-from tensorflow.python.ops.ragged import ragged_tensor_value
+from tensorflow.python.framework import (dtypes, errors, errors_impl, gpu_util,
+                                         importer, ops, random_seed,
+                                         sparse_tensor, tensor_shape,
+                                         tensor_util, versions)
+from tensorflow.python.ops import (array_ops, control_flow_util,
+                                   control_flow_util_v2, script_ops,
+                                   summary_ops_v2, variables)
+from tensorflow.python.ops.ragged import \
+    ragged_ops  # pylint: disable=unused-import
+from tensorflow.python.ops.ragged import ragged_tensor, ragged_tensor_value
 from tensorflow.python.platform import googletest
 from tensorflow.python.platform import tf_logging as logging
 from tensorflow.python.training import server_lib
-from tensorflow.python.util import compat
-from tensorflow.python.util import deprecation
-from tensorflow.python.util import nest
-from tensorflow.python.util import tf_decorator
-from tensorflow.python.util import tf_inspect
+from tensorflow.python.util import (compat, deprecation, nest, tf_decorator,
+                                    tf_inspect)
 from tensorflow.python.util.compat import collections_abc
 from tensorflow.python.util.protobuf import compare
 from tensorflow.python.util.tf_export import tf_export
