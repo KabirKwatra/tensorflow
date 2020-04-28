56f563cfe21a1c22be8525f5cd2a413948b6ee7f
Restyled.io <commits@restyled.io>
2020-04-28 00:20:03 +0000

    Restyled by astyle

--- tensorflow/compiler/mlir/tensorflow/ir/tf_traits.h
+++ tensorflow/compiler/mlir/tensorflow/ir/tf_traits.h
@@ -30,10 +30,10 @@ namespace TF {
 
 // Verifies if 'ref_type' is a REF type corresponding to 'type'.
 static inline LogicalResult VerifyRefTypeMatch(mlir::Type type,
-                                               mlir::Type maybe_ref_type) {
-  if (auto ref_type = maybe_ref_type.dyn_cast<mlir::TF::TensorFlowRefType>())
-    return success(ref_type.RemoveRef().getKind() == type.getKind());
-  return failure();
+        mlir::Type maybe_ref_type) {
+    if (auto ref_type = maybe_ref_type.dyn_cast<mlir::TF::TensorFlowRefType>())
+        return success(ref_type.RemoveRef().getKind() == type.getKind());
+    return failure();
 }
 
 // This class provides verification for ops that are known to have the same
@@ -43,26 +43,26 @@ static inline LogicalResult VerifyRefTypeMatch(mlir::Type type,
 template <typename ConcreteType>
 class OperandsSameAsResultsTypeOrRef
     : public TraitBase<ConcreteType, OperandsSameAsResultsTypeOrRef> {
- public:
-  static LogicalResult verifyTrait(Operation* op) {
-    LogicalResult shapeMatch = impl::verifySameOperandsAndResultShape(op);
-    if (failed(shapeMatch)) return shapeMatch;
-    Type type = op->getResult(0).getType();
-    // Verify that the first result type is same as the rest of the results.
-    // We skip the comparison against itself.
-    for (auto result_type : llvm::drop_begin(op->getResultTypes(), 1)) {
-      if (!mlir::TF::HasCompatibleElementTypes(type, result_type))
-        return op->emitOpError()
-               << "requires all return types to have compatible element types";
+public:
+    static LogicalResult verifyTrait(Operation* op) {
+        LogicalResult shapeMatch = impl::verifySameOperandsAndResultShape(op);
+        if (failed(shapeMatch)) return shapeMatch;
+        Type type = op->getResult(0).getType();
+        // Verify that the first result type is same as the rest of the results.
+        // We skip the comparison against itself.
+        for (auto result_type : llvm::drop_begin(op->getResultTypes(), 1)) {
+            if (!mlir::TF::HasCompatibleElementTypes(type, result_type))
+                return op->emitOpError()
+                       << "requires all return types to have compatible element types";
+        }
+        for (auto operand_type : op->getOperandTypes()) {
+            if (!mlir::TF::HasCompatibleElementTypes(
+                        operand_type, type, /*may_ignore_ref_type_lhs=*/true))
+                return op->emitError() << "requires all operands and results to have "
+                       "compatible element types";
+        }
+        return success();
     }
-    for (auto operand_type : op->getOperandTypes()) {
-      if (!mlir::TF::HasCompatibleElementTypes(
-              operand_type, type, /*may_ignore_ref_type_lhs=*/true))
-        return op->emitError() << "requires all operands and results to have "
-                                  "compatible element types";
-    }
-    return success();
-  }
 };
 
 // Layout agnostic operations do not depend on the operands data layout (data
--- tensorflow/lite/micro/kernels/resize_nearest_neighbor.cc
+++ tensorflow/lite/micro/kernels/resize_nearest_neighbor.cc
@@ -32,79 +32,80 @@ constexpr int kOutputTensor = 0;
 
 TfLiteStatus Prepare(TfLiteContext* context, TfLiteNode* node) {
 #if defined(DEBUG)
-  TF_LITE_ENSURE_EQ(context, NumInputs(node), 2);
-  TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
+    TF_LITE_ENSURE_EQ(context, NumInputs(node), 2);
+    TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
 
-  const TfLiteTensor* input = GetInput(context, node, kInputTensor);
-  const TfLiteTensor* size = GetInput(context, node, kSizeTensor);
-  TfLiteTensor* output = GetOutput(context, node, kOutputTensor);
+    const TfLiteTensor* input = GetInput(context, node, kInputTensor);
+    const TfLiteTensor* size = GetInput(context, node, kSizeTensor);
+    TfLiteTensor* output = GetOutput(context, node, kOutputTensor);
 
-  // Our current implementations rely on the input being 4D,
-  // and the size being 1D tensor with exactly 2 elements.
-  TF_LITE_ENSURE_EQ(context, NumDimensions(input), 4);
-  TF_LITE_ENSURE_EQ(context, NumDimensions(size), 1);
-  TF_LITE_ENSURE_EQ(context, size->type, kTfLiteInt32);
-  TF_LITE_ENSURE_EQ(context, size->dims->data[0], 2);
+    // Our current implementations rely on the input being 4D,
+    // and the size being 1D tensor with exactly 2 elements.
+    TF_LITE_ENSURE_EQ(context, NumDimensions(input), 4);
+    TF_LITE_ENSURE_EQ(context, NumDimensions(size), 1);
+    TF_LITE_ENSURE_EQ(context, size->type, kTfLiteInt32);
+    TF_LITE_ENSURE_EQ(context, size->dims->data[0], 2);
 
-  output->type = input->type;
+    output->type = input->type;
 
-  if (!IsConstantTensor(size)) {
-    TF_LITE_KERNEL_LOG(context,
-                         "Dynamic tensors are unsupported in tfmicro.");
-    return kTfLiteError;
-  }
+    if (!IsConstantTensor(size)) {
+        TF_LITE_KERNEL_LOG(context,
+                           "Dynamic tensors are unsupported in tfmicro.");
+        return kTfLiteError;
+    }
 #endif
-  return kTfLiteOk;
+    return kTfLiteOk;
 }
 
 TfLiteStatus Eval(TfLiteContext* context, TfLiteNode* node) {
-  auto* params =
-      reinterpret_cast<TfLiteResizeNearestNeighborParams*>(node->builtin_data);
+    auto* params =
+        reinterpret_cast<TfLiteResizeNearestNeighborParams*>(node->builtin_data);
 
-  const TfLiteTensor* input = GetInput(context, node, kInputTensor);
-  const TfLiteTensor* size = GetInput(context, node, kSizeTensor);
-  TfLiteTensor* output = GetOutput(context, node, kOutputTensor);
+    const TfLiteTensor* input = GetInput(context, node, kInputTensor);
+    const TfLiteTensor* size = GetInput(context, node, kSizeTensor);
+    TfLiteTensor* output = GetOutput(context, node, kOutputTensor);
 
-  tflite::ResizeNearestNeighborParams op_params;
-  op_params.align_corners = params->align_corners;
-  op_params.half_pixel_centers = false;
+    tflite::ResizeNearestNeighborParams op_params;
+    op_params.align_corners = params->align_corners;
+    op_params.half_pixel_centers = false;
 
-  if (output->type == kTfLiteFloat32) {
-    reference_ops::ResizeNearestNeighbor(
-        op_params, GetTensorShape(input), GetTensorData<int32>(input),
-        GetTensorShape(size), GetTensorData<int32>(size),
-        GetTensorShape(output), GetTensorData<int32>(output));
-  } else if (output->type == kTfLiteUInt8) {
-    reference_ops::ResizeNearestNeighbor(
-        op_params, GetTensorShape(input), GetTensorData<uint8_t>(input),
-        GetTensorShape(size), GetTensorData<int32>(size),
-        GetTensorShape(output), GetTensorData<uint8_t>(output));
-  } else if (output->type == kTfLiteInt8) {
-    reference_ops::ResizeNearestNeighbor(
-        op_params, GetTensorShape(input), GetTensorData<int8_t>(input),
-        GetTensorShape(size), GetTensorData<int32>(size),
-        GetTensorShape(output), GetTensorData<int8_t>(output));
-  } else {
-    TF_LITE_KERNEL_LOG(context,
-                       "Output type is %d, requires float, uint8 or int8.",
-                       output->type);
-    return kTfLiteError;
-  }
+    if (output->type == kTfLiteFloat32) {
+        reference_ops::ResizeNearestNeighbor(
+            op_params, GetTensorShape(input), GetTensorData<int32>(input),
+            GetTensorShape(size), GetTensorData<int32>(size),
+            GetTensorShape(output), GetTensorData<int32>(output));
+    } else if (output->type == kTfLiteUInt8) {
+        reference_ops::ResizeNearestNeighbor(
+            op_params, GetTensorShape(input), GetTensorData<uint8_t>(input),
+            GetTensorShape(size), GetTensorData<int32>(size),
+            GetTensorShape(output), GetTensorData<uint8_t>(output));
+    } else if (output->type == kTfLiteInt8) {
+        reference_ops::ResizeNearestNeighbor(
+            op_params, GetTensorShape(input), GetTensorData<int8_t>(input),
+            GetTensorShape(size), GetTensorData<int32>(size),
+            GetTensorShape(output), GetTensorData<int8_t>(output));
+    } else {
+        TF_LITE_KERNEL_LOG(context,
+                           "Output type is %d, requires float, uint8 or int8.",
+                           output->type);
+        return kTfLiteError;
+    }
 
-  return kTfLiteOk;
+    return kTfLiteOk;
 }
 }  // namespace resize_nearest_neighbor
 
 TfLiteRegistration* Register_RESIZE_NEAREST_NEIGHBOR() {
-  static TfLiteRegistration r = {/*init=*/nullptr,
-                                 /*free=*/nullptr,
-                                 /*prepare=*/resize_nearest_neighbor::Prepare,
-                                 /*invoke=*/resize_nearest_neighbor::Eval,
-                                 /*profiling_string=*/nullptr,
-                                 /*builtin_code=*/0,
-                                 /*custom_name=*/nullptr,
-                                 /*version=*/0};
-  return &r;
+    static TfLiteRegistration r = {/*init=*/nullptr,
+                                            /*free=*/nullptr,
+                                            /*prepare=*/resize_nearest_neighbor::Prepare,
+                                            /*invoke=*/resize_nearest_neighbor::Eval,
+                                            /*profiling_string=*/nullptr,
+                                            /*builtin_code=*/0,
+                                            /*custom_name=*/nullptr,
+                                            /*version=*/0
+                                  };
+    return &r;
 }
 
 }  // namespace micro
