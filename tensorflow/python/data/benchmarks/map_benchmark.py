3f3c1691289e1837c4ea5802b4fe39dfa9d5213d
Restyled.io <commits@restyled.io>
2020-04-29 04:20:01 +0000

    Restyled by autopep8

--- tensorflow/python/data/benchmarks/map_benchmark.py
+++ tensorflow/python/data/benchmarks/map_benchmark.py
@@ -30,100 +30,102 @@ from tensorflow.python.ops import random_ops
 
 # TODO(b/119837791): Add eager benchmarks.
 class MapBenchmark(benchmark_base.DatasetBenchmarkBase):
-  """Benchmarks for `tf.data.Dataset.map()`."""
+    """Benchmarks for `tf.data.Dataset.map()`."""
 
-  def benchmark_chain_of_maps(self):
+    def benchmark_chain_of_maps(self):
 
-    def benchmark_helper(chain_length, fn, use_inter_op_parallelism, label):
-      dataset = dataset_ops.Dataset.range(10000)
-      for _ in range(chain_length):
+        def benchmark_helper(chain_length, fn, use_inter_op_parallelism, label):
+            dataset = dataset_ops.Dataset.range(10000)
+            for _ in range(chain_length):
+                dataset = dataset_ops.MapDataset(
+                    dataset, fn, use_inter_op_parallelism=use_inter_op_parallelism)
+            self.run_and_report_benchmark(
+                dataset,
+                num_elements=10000,
+                name="chain_length_%d%s" % (chain_length, label))
+
+        chain_lengths = [0, 1, 2, 5, 10, 20, 50]
+        for chain_length in chain_lengths:
+            benchmark_helper(chain_length, lambda x: x + 1, True, "")
+            benchmark_helper(chain_length, lambda x: x +
+                             1, False, "_single_threaded")
+            benchmark_helper(chain_length, lambda x: x, True, "_short_circuit")
+
+    def benchmark_map_fan_out(self):
+        fan_outs = [1, 2, 5, 10, 20, 50, 100]
+
+        def benchmark_helper(fan_out, fn, use_inter_op_parallelism, label):
+            dataset = dataset_ops.Dataset.from_tensors(
+                tuple(0 for _ in range(fan_out))).repeat(None)
+            dataset = dataset_ops.MapDataset(
+                dataset, fn, use_inter_op_parallelism=use_inter_op_parallelism)
+            self.run_and_report_benchmark(
+                dataset,
+                num_elements=10000,
+                name="fan_out_%d%s" % (fan_out, label))
+
+        for fan_out in fan_outs:
+            benchmark_helper(
+                fan_out, lambda *xs: [x + 1 for x in xs], True, "")
+            benchmark_helper(fan_out, lambda *xs: [x + 1 for x in xs], False,
+                             "_single_threaded")
+            benchmark_helper(fan_out, lambda *xs: xs, True, "_short_circuit")
+
+    def benchmark_stats(self):
+        for stats in [True, False]:
+            dataset = dataset_ops.Dataset.range(1000).repeat()
+            dataset = dataset.map(lambda x: x + 1, num_parallel_calls=32)
+            options = dataset_ops.Options()
+            options.experimental_deterministic = False
+            if stats:
+                aggregator = stats_aggregator.StatsAggregator()
+                options.experimental_stats.aggregator = aggregator
+            dataset = dataset.with_options(options)
+            self.run_and_report_benchmark(
+                dataset, num_elements=10000, name="stats_%s" % stats)
+
+    def benchmark_sequential_control_flow(self):
+        dataset = dataset_ops.Dataset.from_tensors(100000)
+
+        def fn(x):
+            i = constant_op.constant(0)
+
+            def body(i, x):
+                return math_ops.add(i, 1), x
+
+            return control_flow_ops.while_loop(math_ops.less, body, [i, x])
+
+        dataset = dataset.map(fn)
+        self.run_and_report_benchmark(
+            dataset,
+            num_elements=1,
+            name="sequential_control_flow",
+            apply_default_optimizations=True)
+
+    def benchmark_parallel_control_flow(self):
+        dataset = dataset_ops.Dataset.from_tensors(
+            random_ops.random_uniform([100, 10000000]))
+
+        def fn(x):
+            return map_fn.map_fn(
+                lambda y: y * array_ops.transpose(y), x, parallel_iterations=10)
+
+        dataset = dataset.map(fn)
+        self.run_and_report_benchmark(
+            dataset,
+            num_elements=1,
+            name="parallel_control_flow",
+            apply_default_optimizations=True)
+
+    def benchmark_execution_overhead(self):
+        dataset = dataset_ops.Dataset.range(100000)
         dataset = dataset_ops.MapDataset(
-            dataset, fn, use_inter_op_parallelism=use_inter_op_parallelism)
-      self.run_and_report_benchmark(
-          dataset,
-          num_elements=10000,
-          name="chain_length_%d%s" % (chain_length, label))
-
-    chain_lengths = [0, 1, 2, 5, 10, 20, 50]
-    for chain_length in chain_lengths:
-      benchmark_helper(chain_length, lambda x: x + 1, True, "")
-      benchmark_helper(chain_length, lambda x: x + 1, False, "_single_threaded")
-      benchmark_helper(chain_length, lambda x: x, True, "_short_circuit")
-
-  def benchmark_map_fan_out(self):
-    fan_outs = [1, 2, 5, 10, 20, 50, 100]
-
-    def benchmark_helper(fan_out, fn, use_inter_op_parallelism, label):
-      dataset = dataset_ops.Dataset.from_tensors(
-          tuple(0 for _ in range(fan_out))).repeat(None)
-      dataset = dataset_ops.MapDataset(
-          dataset, fn, use_inter_op_parallelism=use_inter_op_parallelism)
-      self.run_and_report_benchmark(
-          dataset,
-          num_elements=10000,
-          name="fan_out_%d%s" % (fan_out, label))
-
-    for fan_out in fan_outs:
-      benchmark_helper(fan_out, lambda *xs: [x + 1 for x in xs], True, "")
-      benchmark_helper(fan_out, lambda *xs: [x + 1 for x in xs], False,
-                       "_single_threaded")
-      benchmark_helper(fan_out, lambda *xs: xs, True, "_short_circuit")
-
-  def benchmark_stats(self):
-    for stats in [True, False]:
-      dataset = dataset_ops.Dataset.range(1000).repeat()
-      dataset = dataset.map(lambda x: x + 1, num_parallel_calls=32)
-      options = dataset_ops.Options()
-      options.experimental_deterministic = False
-      if stats:
-        aggregator = stats_aggregator.StatsAggregator()
-        options.experimental_stats.aggregator = aggregator
-      dataset = dataset.with_options(options)
-      self.run_and_report_benchmark(
-          dataset, num_elements=10000, name="stats_%s" % stats)
-
-  def benchmark_sequential_control_flow(self):
-    dataset = dataset_ops.Dataset.from_tensors(100000)
-
-    def fn(x):
-      i = constant_op.constant(0)
-
-      def body(i, x):
-        return math_ops.add(i, 1), x
-
-      return control_flow_ops.while_loop(math_ops.less, body, [i, x])
-
-    dataset = dataset.map(fn)
-    self.run_and_report_benchmark(
-        dataset,
-        num_elements=1,
-        name="sequential_control_flow",
-        apply_default_optimizations=True)
-
-  def benchmark_parallel_control_flow(self):
-    dataset = dataset_ops.Dataset.from_tensors(
-        random_ops.random_uniform([100, 10000000]))
-
-    def fn(x):
-      return map_fn.map_fn(
-          lambda y: y * array_ops.transpose(y), x, parallel_iterations=10)
-
-    dataset = dataset.map(fn)
-    self.run_and_report_benchmark(
-        dataset,
-        num_elements=1,
-        name="parallel_control_flow",
-        apply_default_optimizations=True)
-
-  def benchmark_execution_overhead(self):
-    dataset = dataset_ops.Dataset.range(100000)
-    dataset = dataset_ops.MapDataset(
-        dataset, lambda x: x + 1, use_inter_op_parallelism=False)
-    self.run_and_report_benchmark(
-        dataset,
-        num_elements=100000,
-        name="execution_overhead")
+            dataset, lambda x: x + 1, use_inter_op_parallelism=False)
+        self.run_and_report_benchmark(
+            dataset,
+            num_elements=100000,
+            name="execution_overhead")
 
 
 if __name__ == "__main__":
-  benchmark_base.test.main()
+    benchmark_base.test.main()
--- tensorflow/python/keras/layers/preprocessing/text_vectorization_distribution_test.py
+++ tensorflow/python/keras/layers/preprocessing/text_vectorization_distribution_test.py
@@ -35,10 +35,10 @@ from tensorflow.python.platform import test
 
 
 def get_layer_class():
-  if context.executing_eagerly():
-    return text_vectorization.TextVectorization
-  else:
-    return text_vectorization_v1.TextVectorization
+    if context.executing_eagerly():
+        return text_vectorization.TextVectorization
+    else:
+        return text_vectorization_v1.TextVectorization
 
 
 @combinations.generate(
@@ -46,61 +46,63 @@ def get_layer_class():
         distribution=strategy_combinations.all_strategies,
         mode=["eager", "graph"]))
 class TextVectorizationDistributionTest(
-    keras_parameterized.TestCase,
-    preprocessing_test_utils.PreprocessingLayerTest):
+        keras_parameterized.TestCase,
+        preprocessing_test_utils.PreprocessingLayerTest):
 
-  def test_distribution_strategy_output(self, distribution):
-    vocab_data = ["earth", "wind", "and", "fire"]
-    input_array = np.array([["earth", "wind", "and", "fire"],
-                            ["fire", "and", "earth", "michigan"]])
-    input_dataset = dataset_ops.Dataset.from_tensor_slices(input_array).batch(
-        2, drop_remainder=True)
+    def test_distribution_strategy_output(self, distribution):
+        vocab_data = ["earth", "wind", "and", "fire"]
+        input_array = np.array([["earth", "wind", "and", "fire"],
+                                ["fire", "and", "earth", "michigan"]])
+        input_dataset = dataset_ops.Dataset.from_tensor_slices(input_array).batch(
+            2, drop_remainder=True)
 
-    expected_output = [[2, 3, 4, 5], [5, 4, 2, 1]]
+        expected_output = [[2, 3, 4, 5], [5, 4, 2, 1]]
 
-    config.set_soft_device_placement(True)
+        config.set_soft_device_placement(True)
 
-    with distribution.scope():
-      input_data = keras.Input(shape=(None,), dtype=dtypes.string)
-      layer = get_layer_class()(
-          max_tokens=None,
-          standardize=None,
-          split=None,
-          output_mode=text_vectorization.INT)
-      layer.set_vocabulary(vocab_data)
-      int_data = layer(input_data)
-      model = keras.Model(inputs=input_data, outputs=int_data)
+        with distribution.scope():
+            input_data = keras.Input(shape=(None,), dtype=dtypes.string)
+            layer = get_layer_class()(
+                max_tokens=None,
+                standardize=None,
+                split=None,
+                output_mode=text_vectorization.INT)
+            layer.set_vocabulary(vocab_data)
+            int_data = layer(input_data)
+            model = keras.Model(inputs=input_data, outputs=int_data)
 
-    output_dataset = model.predict(input_dataset)
-    self.assertAllEqual(expected_output, output_dataset)
+        output_dataset = model.predict(input_dataset)
+        self.assertAllEqual(expected_output, output_dataset)
 
-  def test_distribution_strategy_output_with_adapt(self, distribution):
-    vocab_data = [[
-        "earth", "earth", "earth", "earth", "wind", "wind", "wind", "and",
-        "and", "fire"
-    ]]
-    vocab_dataset = dataset_ops.Dataset.from_tensors(vocab_data)
-    input_array = np.array([["earth", "wind", "and", "fire"],
-                            ["fire", "and", "earth", "michigan"]])
-    input_dataset = dataset_ops.Dataset.from_tensor_slices(input_array).batch(
-        2, drop_remainder=True)
+    def test_distribution_strategy_output_with_adapt(self, distribution):
+        vocab_data = [[
+            "earth", "earth", "earth", "earth", "wind", "wind", "wind", "and",
+            "and", "fire"
+        ]]
+        vocab_dataset = dataset_ops.Dataset.from_tensors(vocab_data)
+        input_array = np.array([["earth", "wind", "and", "fire"],
+                                ["fire", "and", "earth", "michigan"]])
+        input_dataset = dataset_ops.Dataset.from_tensor_slices(input_array).batch(
+            2, drop_remainder=True)
 
-    expected_output = [[2, 3, 4, 5], [5, 4, 2, 1]]
+        expected_output = [[2, 3, 4, 5], [5, 4, 2, 1]]
 
-    config.set_soft_device_placement(True)
+        config.set_soft_device_placement(True)
 
-    with distribution.scope():
-      input_data = keras.Input(shape=(None,), dtype=dtypes.string)
-      layer = get_layer_class()(
-          max_tokens=None,
-          standardize=None,
-          split=None,
-          output_mode=text_vectorization.INT)
-      layer.adapt(vocab_dataset)
-      int_data = layer(input_data)
-      model = keras.Model(inputs=input_data, outputs=int_data)
+        with distribution.scope():
+            input_data = keras.Input(shape=(None,), dtype=dtypes.string)
+            layer = get_layer_class()(
+                max_tokens=None,
+                standardize=None,
+                split=None,
+                output_mode=text_vectorization.INT)
+            layer.adapt(vocab_dataset)
+            int_data = layer(input_data)
+            model = keras.Model(inputs=input_data, outputs=int_data)
 
-    output_dataset = model.predict(input_dataset)
-    self.assertAllEqual(expected_output, output_dataset)
+        output_dataset = model.predict(input_dataset)
+        self.assertAllEqual(expected_output, output_dataset)
+
+
 if __name__ == "__main__":
-  test.main()
+    test.main()
